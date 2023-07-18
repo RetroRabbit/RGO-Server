156700e161cf72b7f8b709ce512d700abf5be73f
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-07-18 12:52:43 +0200

    Removing Comments

--- RGO Backend/Program.cs
+++ RGO Backend/Program.cs
@@ -13,10 +13,7 @@ namespace ROG.App
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -27,7 +24,6 @@ namespace ROG.App
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
