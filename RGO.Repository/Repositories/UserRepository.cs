b247c55e4d2e7f835ea4e7bf1bcdc39ef939fab9
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-07-25 14:48:12 +0200

    Adding Repository for users

---
+++ RGO.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RGO.Domain.Interfaces.Repository;
+using RGO.Domain.Models;
+using RGO.Repository.Entities;
+
+namespace RGO.Repository.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public UserRepository(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public async Task<UserDto> GetUserByEmail(string email)
+        {
+            bool userExists = await UserExists(email);
+
+            if (!userExists) throw new Exception("User not found");
+
+            User user = await _databaseContext.users.FirstAsync(u => u.email == email);
+
+            return user.ToDTO();
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            bool userExists = await _databaseContext.users.AnyAsync(u => u.email == email);
+
+            return userExists;
+        }
+    }
+}
