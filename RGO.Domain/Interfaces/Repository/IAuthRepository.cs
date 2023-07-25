9e93dd200b0bda3dea2992877aacafb6b8aa2cfb
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-07-25 15:11:10 +0200

    Modify AuthRepository

--- RGO.Domain/Interfaces/Repository/IAuthRepository.cs
+++ RGO.Domain/Interfaces/Repository/IAuthRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace RGO.Domain.Interfaces.Repository
+﻿using RGO.Domain.Models;
+
+namespace RGO.Domain.Interfaces.Repository
 {
     public interface IAuthRepository
     {
         Task<bool> FindUserByEmail(string email);
+        Task<UserDto> GetUserByEmail(string email);
     }
 }
