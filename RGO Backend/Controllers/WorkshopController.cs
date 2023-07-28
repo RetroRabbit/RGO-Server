3fdd4e8d1376e1a7fd16e69c195a531232f6c346
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-07-28 10:24:50 +0200

    Created base controller for Workshops

---
+++ RGO Backend/Controllers/WorkshopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RGO_Backend.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class WorkshopController : ControllerBase
+    {
+        [HttpGet("workshops")]
+        public async Task<IActionResult> GetWorkShops()
+        {
+            var workshops = new List<string>() { "Workshop 1", "Workshop 2", "Workshop 3" };
+            return Ok(workshops);
+        }
+    }
+}
