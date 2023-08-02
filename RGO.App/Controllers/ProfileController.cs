f424317f396f7b58e01aa9cd6420d823eee4755d
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-02 12:14:06 +0200

    Changing namespaces to follow naming convention
    Renamed 'RGO Backend' to 'RGO.App' and making sure controllers match this change

--- RGO.App/Controllers/ProfileController.cs
+++ RGO.App/Controllers/ProfileController.cs
@@ -3,7 +3,7 @@ using RGO.Domain.Interfaces.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace RGO_Backend.Controllers
+namespace RGO.App.Controllers
 {
     [Route("[controller]")]
     [ApiController]
