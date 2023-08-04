6199f0607242b0988bc0cca5bc2cef8a3256dfa2
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-04 14:52:26 +0200

    implementing repositories

---
+++ RGO.Repository/Repositories/StackRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RGO.Domain.Interfaces.Repository;
+using RGO.Domain.Models;
+using RGO.Repository.Entities;
+
+namespace RGO.Repository.Repositories
+{
+    public class StackRepository : IStackRepository
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public StackRepository(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public async Task<StacksDto[]> GetBackendStack()
+        {
+            StacksDto[] stacks = await _databaseContext.stacks
+                .Where(x => x.StackType == 2)
+                .Select(x => x.ToDTO())
+                .ToArrayAsync();
+            return stacks;
+        }
+
+        public async Task<StacksDto[]> GetDatabaseStack()
+        {
+            StacksDto[] stacks = await _databaseContext.stacks
+                .Where(x => x.StackType == 0)
+                .Select(x => x.ToDTO())
+                .ToArrayAsync();
+            return stacks;
+        }
+
+        public async Task<StacksDto[]> GetFrontendStack()
+        {
+            StacksDto[] stacks = await _databaseContext.stacks
+                .Where(x => x.StackType == 1)
+                .Select(x => x.ToDTO())
+                .ToArrayAsync();
+            return stacks;
+        }
+
+        public async Task<StacksDto> GetStack(int id)
+        {
+            Stacks? stack = await _databaseContext.stacks
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (stack == null) 
+                throw new Exception("Stack not found");
+
+            return stack.ToDTO();
+        }
+
+        public async Task<bool> StackExists(int id)
+        {
+            bool found = await _databaseContext.stacks
+                .AnyAsync(x => x.Id == id);
+            return found;
+        }
+    }
+}
---
+++ RGO.Repository/Repositories/UserStackRepository.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RGO.Domain.Interfaces.Repository;
+using RGO.Domain.Models;
+using RGO.Repository.Entities;
+
+namespace RGO.Repository.Repositories
+{
+    public class UserStackRepository : IUserStackRepository
+    {
+        private readonly DatabaseContext _databaseContext;
+        private readonly IStackRepository _stackRepository;
+
+        public UserStackRepository(DatabaseContext databaseContext, IStackRepository stackRepository)
+        {
+            _databaseContext = databaseContext;
+            _stackRepository = stackRepository;
+        }
+
+        public async Task<UserStackDto> CreateUserStack(int userId)
+        {
+            Random random = new Random();
+
+            StacksDto[] backendStack = await _stackRepository.GetBackendStack();
+            int backendStackId = backendStack[random.Next(0, backendStack.Length)].Id;
+            StacksDto[] frontendStack = await _stackRepository.GetFrontendStack();
+            int frontendStackId = frontendStack[random.Next(0, frontendStack.Length)].Id;
+            StacksDto[] databaseStack = await _stackRepository.GetDatabaseStack();
+            int databaseStackId = databaseStack[random.Next(0, databaseStack.Length)].Id;
+
+            UserStackDto newUserStack = new UserStackDto
+            (
+                0,
+                userId,
+                backendStackId,
+                frontendStackId,
+                databaseStackId,
+                "Personal project tech stack default text.",
+                1,
+                DateTime.UtcNow);
+
+            try
+            {
+                await _databaseContext.userStacks.AddAsync(new UserStacks(newUserStack));
+                await _databaseContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to create Tech Stack for User {userId}.(Error: {ex.Message})");
+            }
+
+            UserStacks? stack = await _databaseContext.userStacks
+                .Include(x => x.User)
+                .Include(x => x.BackendUserStack)
+                .Include(x => x.FrontendUserStack)
+                .Include(x => x.DatabaseUserStack)
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if (stack == null)
+                throw new Exception("An error occured making a Tech Stack");
+
+            return stack.ToDTO();
+        }
+
+        public async Task<UserStackDto> GetUserStack(int userId)
+        {
+            UserStacks? userStack = await _databaseContext.userStacks
+                .Include(x => x.User)
+                .Include(x => x.BackendUserStack)
+                .Include(x => x.FrontendUserStack)
+                .Include(x => x.DatabaseUserStack)
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if (userStack == null)
+                throw new Exception("User stack not found");
+
+            return userStack.ToDTO();
+        }
+
+        public async Task<bool> HasTechStack(int userId)
+        {
+            bool found = await _databaseContext.userStacks.AnyAsync(x => x.UserId == userId);
+            return found;
+        }
+
+        public async Task<UserStackDto> RemoveUserStack(int userId)
+        {
+            UserStacks? userStack = await _databaseContext.userStacks
+                .Include(x => x.User)
+                .Include(x => x.BackendUserStack)
+                .Include(x => x.FrontendUserStack)
+                .Include(x => x.DatabaseUserStack)
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if (userStack == null)
+                throw new Exception("User stack not found");
+
+            _databaseContext.userStacks.Remove(userStack);
+            await _databaseContext.SaveChangesAsync();
+
+            return userStack.ToDTO();
+        }
+
+        public async Task<UserStackDto> UpdateUserStack(int userId, string description)
+        {
+            UserStacks? userStack = await _databaseContext.userStacks
+                .Include(x => x.User)
+                .Include(x => x.BackendUserStack)
+                .Include(x => x.FrontendUserStack)
+                .Include(x => x.DatabaseUserStack)
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if (userStack == null)
+                throw new Exception("User stack not found");
+
+            userStack.Description = description;
+
+            _databaseContext.userStacks.Update(userStack);
+            await _databaseContext.SaveChangesAsync();
+
+            return userStack.ToDTO();
+        }
+    }
+}
