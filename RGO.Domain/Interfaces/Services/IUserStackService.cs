23e2d87b78d2afccf4dc824232eb41bbc90405b4
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-04 14:50:25 +0200

    Adding user stack service

---
+++ RGO.Domain/Interfaces/Services/IUserStackService.cs
@@ -0,0 +1,14 @@
+﻿using RGO.Domain.Models;
+
+namespace RGO.Domain.Interfaces.Services
+{
+    public interface IUserStackService
+    {
+        Task<bool> HasTechStack(int userId);
+        Task<UserStackDto> GetUserStack(int userId);
+        Task<UserStackDto> CreateUserStack(int userId);
+        Task<UserStackDto> RemoveUserStack(int userId);
+
+        Task<UserStackDto> UpdateUserStack(int userId, string description);
+    }
+}
---
+++ RGO.Domain/Services/UserStackService.cs
@@ -0,0 +1,46 @@
+﻿using RGO.Domain.Interfaces.Repository;
+using RGO.Domain.Interfaces.Services;
+using RGO.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGO.Domain.Services
+{
+    public class UserStackService : IUserStackService
+    {
+        private readonly IUserStackRepository _userStackRepository;
+
+        public UserStackService(IUserStackRepository userStackRepository)
+        {
+            _userStackRepository = userStackRepository;
+        }
+
+        public Task<UserStackDto> CreateUserStack(int userId)
+        {
+            return _userStackRepository.CreateUserStack(userId);
+        }
+
+        public Task<UserStackDto> GetUserStack(int userId)
+        {
+            return _userStackRepository.GetUserStack(userId);
+        }
+
+        public Task<bool> HasTechStack(int userId)
+        {
+            return _userStackRepository.HasTechStack(userId);
+        }
+
+        public Task<UserStackDto> RemoveUserStack(int userId)
+        {
+            return _userStackRepository.RemoveUserStack(userId);
+        }
+
+        public Task<UserStackDto> UpdateUserStack(int userId, string description)
+        {
+            return _userStackRepository.UpdateUserStack(userId, description);
+        }
+    }
+}
