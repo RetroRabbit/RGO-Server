dc60994a013689648c76d4db780cb417f7ee716e
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-04 14:51:59 +0200

    Adding UserStackController

---
+++ RGO.App/Controllers/UserStackController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RGO.Domain.Interfaces.Services;
+
+namespace RGO.App.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserStackController : ControllerBase
+    {
+        private readonly IUserStackService _userStackService;
+
+        public UserStackController(IUserStackService userStackService)
+        {
+            _userStackService = userStackService;
+        }
+
+        [HttpGet("GetUserStack")]
+        public async Task<IActionResult> GetUserStack([FromQuery] int userId)
+        {
+            try
+            {
+                var userStack = await _userStackService.GetUserStack(userId);
+                return Ok(userStack);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("CreateUserStack")]
+        public async Task<IActionResult> CreateUserStack([FromQuery] int userId)
+        {
+            try
+            {
+                var userStack = await _userStackService.CreateUserStack(userId);
+                return Ok(userStack);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("RemoveUserStack")]
+        public async Task<IActionResult> RemoveUserStack([FromQuery] int userId)
+        {
+            try
+            {
+                var userStack = await _userStackService.RemoveUserStack(userId);
+                return Ok(userStack);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("UpdateUserStack")]
+        public async Task<IActionResult> UpdateUserStack([FromQuery] int userId, [FromBody] string description)
+        {
+            try
+            {
+                var userStack = await _userStackService.UpdateUserStack(userId, description);
+                return Ok(userStack);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
