0dd068b02d849aff0a9762e035909bf9245333f5
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-04 14:26:53 +0200

    Add Repository interfaces

---
+++ RGO.Domain/Interfaces/Repository/IStackRepository.cs
@@ -0,0 +1,13 @@
+﻿using RGO.Domain.Models;
+
+namespace RGO.Domain.Interfaces.Repository
+{
+    public interface IStackRepository
+    {
+        Task<bool> StackExists(int id);
+        Task<StacksDto> GetStack(int id);
+        Task<StacksDto[]> GetBackendStack();
+        Task<StacksDto[]> GetFrontendStack();
+        Task<StacksDto[]> GetDatabaseStack();
+    }
+}
