f1bf04a985ae16dfc55827373d7005dc0c43b248
CarlWehl <Cwehl@retrorabbit.co.za>
2023-08-07 14:02:00 +0200

    Clean Up unused Imports

--- RGO.Repository/Repositories/ProfileRepository.cs
+++ RGO.Repository/Repositories/ProfileRepository.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RGO.Domain.Interfaces.Repository;
 using RGO.Domain.Models;
-using RGO.Repository.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RGO.Repository.Repositories
 {
