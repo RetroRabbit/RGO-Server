7643f151d8c44c4414c0d129e81633bfff7244b3
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-08 13:02:55 +0200

    Adding roles

---
+++ RGO.Repository/Entities/Role.cs
@@ -0,0 +1,41 @@
+﻿using RGO.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RGO.Repository.Entities
+{
+    public class Role
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        /*[Column("userId")]
+        [ForeignKey("User")]
+        public int UserId { get; set; }*/
+
+        [Column("Description")]
+        public string Description { get; set; }
+
+        /*public virtual User User { get; set; }*/
+        
+        public Role()
+        {
+        }
+
+        /*public Role(RoleDto roleDto)
+        {
+            Id = roleDto.Id;
+            UserId = roleDto.UserId;
+            Description = roleDto.Description;
+        }
+
+        public RoleDto ToDTO()
+        {
+            return new RoleDto(
+                Id,
+                UserId,
+                Description);
+        }*/
+    }
+}
---
+++ RGO.Repository/Entities/UserRole.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RGO.Repository.Entities
+{
+    public class UserRole
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("userId")]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [Column("roleId")]
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
