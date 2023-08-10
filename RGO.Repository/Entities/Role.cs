86f8de75f157abced715f8eaa591c4242f97ed7d
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-10 10:30:45 +0200

    Removing comments

--- RGO.Repository/Entities/Role.cs
+++ RGO.Repository/Entities/Role.cs
@@ -10,32 +10,7 @@ namespace RGO.Repository.Entities
         [Column("id")]
         public int Id { get; set; }
 
-        /*[Column("userId")]
-        [ForeignKey("User")]
-        public int UserId { get; set; }*/
-
         [Column("Description")]
         public string Description { get; set; }
-
-        /*public virtual User User { get; set; }*/
-        
-        public Role()
-        {
-        }
-
-        /*public Role(RoleDto roleDto)
-        {
-            Id = roleDto.Id;
-            UserId = roleDto.UserId;
-            Description = roleDto.Description;
-        }
-
-        public RoleDto ToDTO()
-        {
-            return new RoleDto(
-                Id,
-                UserId,
-                Description);
-        }*/
     }
 }
