9dc856c2984fb42026313740f6863f3f59b66fda
DewanAlberts <dalberts@retrorabbit.co.za>
2023-08-11 13:48:22 +0200

    Saving database changes

--- RGO.Repository/Repositories/UserRepository.cs
+++ RGO.Repository/Repositories/UserRepository.cs
@@ -14,7 +14,6 @@ namespace RGO.Repository.Repositories
         {
             _databaseContext = databaseContext;
         }
-
         public async Task<bool> UserExists(string email)
         {
             bool userExists = await _databaseContext.users.AnyAsync(u => u.Email == email);
@@ -47,11 +46,18 @@ namespace RGO.Repository.Repositories
         public async Task<UserDto> UpdateUser(string email, ProfileDto updatedProfile)
         {
             User? existingUser = await _databaseContext.users
+                .Include(u => u.Skills)
+                .Include(u => u.Socials)
+                .Include(u => u.UserProjects)
+                .Include(u => u.UserCertifications)
                 .FirstOrDefaultAsync(u => u.Email == email);
 
             if (existingUser == null)
+            {
                 throw new Exception("User not found");
+            }
 
+            // Update user properties
             existingUser.FirstName = updatedProfile.FirstName;
             existingUser.LastName = updatedProfile.LastName;
             existingUser.Email = updatedProfile.Email;
@@ -62,89 +68,62 @@ namespace RGO.Repository.Repositories
             existingUser.Level = updatedProfile.Level;
             existingUser.Phone = updatedProfile.Phone;
 
-            var currUser = _databaseContext.users.Update(existingUser);
+            // Clear existing related entities
+            existingUser.Skills.Clear();
+            existingUser.Socials.Clear();
+            existingUser.UserProjects.Clear();
+            existingUser.UserCertifications.Clear();
 
-            List<Skill> skills = await _databaseContext.skill
-                .Where(s => s.UserId == existingUser.Id)
-                .ToListAsync();
-            foreach (Skill skill in skills)
+            foreach (var updatedSkill in updatedProfile.Skills)
             {
-                bool skillExists = updatedProfile.Skills.Any(s =>
-                s.Id == skill.Id &&
-                s.Title == skill.Title &&
-                s.Description == skill.Description &&
-                s.UserId == existingUser.Id);
-
-                if (!skillExists)
+                existingUser.Skills.Add(new Skill
                 {
-                    _databaseContext.skill.Update(skill);
-                }
-                // await _databaseContext.SaveChangesAsync();
+                    Id = updatedSkill.Id,
+                    UserId = updatedSkill.UserId,
+                    Title = updatedSkill.Title,
+                    Description = updatedSkill.Description,
+                });
             }
-
-            List<Social> socials = await _databaseContext.social
-                .Where(s => s.UserId == existingUser.Id)
-                .ToListAsync();
-
-            foreach (Social social in socials)
+            foreach (var updatedSocial in updatedProfile.Socials)
             {
-                bool socialExists = updatedProfile.Socials.Any(s =>
-                    s.Id == social.Id
-                    && s.CodeWars == social.CodeWars
-                    && s.Discord == social.Discord
-                    && s.GitHub == social.GitHub
-                    && s.LinkedIn == social.LinkedIn
-                    && s.UserId == existingUser.Id);
-
-                if (!socialExists)
+                existingUser.Socials.Add(new Social
                 {
-                    _databaseContext.social.Update(social);
-                    // await _databaseContext.SaveChangesAsync();
-                }
+                    Id = updatedSocial.Id,
+                    UserId = updatedSocial.UserId,
+                    CodeWars = updatedSocial.CodeWars,
+                    Discord = updatedSocial.Discord,
+                    GitHub = updatedSocial.GitHub,
+                    LinkedIn = updatedSocial.LinkedIn,
+                });
             }
-
-            List<Certifications> certifications = await _databaseContext.certifications
-                .Where(c => c.UserId == existingUser.Id)
-                .ToListAsync();
-
-            foreach (Certifications certification in certifications)
+            foreach (var updatedUserProjects in updatedProfile.Projects)
             {
-                bool certificationExists = updatedProfile.Certifications.Any(c =>
-                    c.Id == certification.Id
-                    && c.Title == certification.Title
-                    && c.Description == certification.Description
-                    && c.UserId == existingUser.Id);
-
-                if (!certificationExists)
+                existingUser.UserProjects.Add(new Projects
                 {
-                    _databaseContext.certifications.Update(certification);
-                    //await _databaseContext.SaveChangesAsync();
-                }
+                    Id=updatedUserProjects.Id, 
+                    UserId = updatedUserProjects.UserId,
+                    Name = updatedUserProjects.Name,
+                    Description = updatedUserProjects.Description,
+                    Role= updatedUserProjects.Role,
+                });
             }
-
-            List<Projects> projects = await _databaseContext.projects
-                .Where(p => p.UserId == existingUser.Id)
-                .ToListAsync();
-            foreach (Projects project in projects)
+            foreach (var updatedUserCertifications in updatedProfile.Certifications)
             {
-                  bool projectExists = updatedProfile.Projects.Any(p =>
-                    p.Id == project.Id
-                    && p.Name == project.Name
-                    && p.Role == project.Role
-                    && p.Description == project.Description
-                    && p.UserId == existingUser.Id);
-
-                if (!projectExists)
+                existingUser.UserCertifications.Add(new Certifications
                 {
-                    _databaseContext.projects.Update(project);
-                    // await _databaseContext.SaveChangesAsync();
-                }
+                    Id = updatedUserCertifications.Id,
+                    UserId = updatedUserCertifications.UserId,
+                    Title= updatedUserCertifications.Title,
+                    Description = updatedUserCertifications.Description,  
+                });
             }
-
+            // Update the existing user entity
+            _databaseContext.users.Update(existingUser);
             await _databaseContext.SaveChangesAsync();
 
-            return currUser.Entity.ToDTO();
+            return existingUser.ToDTO();
         }
+
         public async Task<List<UserDto>> GetUsers()
         {
             var allUsers = await _databaseContext.users.ToListAsync();
