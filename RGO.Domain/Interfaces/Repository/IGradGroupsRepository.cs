7702429b3cbe6f3d2462b59e4f4447c8e03f3d43
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-08-14 14:50:03 +0000

    Apply suggestions from code review

--- RGO.Domain/Interfaces/Repository/IGradGroupsRepository.cs
+++ RGO.Domain/Interfaces/Repository/IGradGroupsRepository.cs
@@ -5,7 +5,7 @@ namespace RGO.Domain.Interfaces.Repository;
 public interface IGradGroupsRepository
 {
     /// <summary>
-    /// Retrieve User Groups
+    /// Retrieve Grad Groups
     /// </summary>
     /// <returns>List of User Groups</returns>
     public Task<List<GradGroupDto>> GetGradGroups();
