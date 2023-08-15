03628dc6bdfd6d8a7c82c987357f94dc4e6c1b5e
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-15 09:16:01 +0200

    Add Documentation

--- RGO.Domain/Interfaces/Services/IAuthService.cs
+++ RGO.Domain/Interfaces/Services/IAuthService.cs
@@ -4,8 +4,24 @@ namespace RGO.Domain.Interfaces.Services;
 
 public interface IAuthService
 {
+    /// <summary>
+    /// Check if user exist
+    /// </summary>
+    /// <param name="email"></param>
+    /// <returns></returns>
     Task<bool> CheckUserExist(string email);
 
+    /// <summary>
+    /// Get user roles by email
+    /// </summary>
+    /// <param name="email"></param>
+    /// <returns>List of role, will reflect as list of int</returns>
     Task<List<UserRole>> GetUserRoles(string email);
+
+    /// <summary>
+    /// Generate JWT token
+    /// </summary>
+    /// <param name="email"></param>
+    /// <returns>JWT token as string</returns>
     Task<string> GenerateToken(string email);
 }
