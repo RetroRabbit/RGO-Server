b8653dd96f602d657ebd3ab7247544947a863294
CarlWehl <Cwehl@retrorabbit.co.za>
2023-08-15 14:28:58 +0200

    Added docks to interface classes

--- RGO.Domain/Interfaces/Repository/IGradEventsRepository.cs
+++ RGO.Domain/Interfaces/Repository/IGradEventsRepository.cs
@@ -4,6 +4,10 @@ namespace RGO.Domain.Interfaces.Repository;
 
 public interface IGradEventsRepository
 {
+    /// <summary>
+    /// Gets all events
+    /// </summary>
+    /// <returns>all events</returns>
     Task<GradEventsDto[]> GetAllEvents();
 }
 
