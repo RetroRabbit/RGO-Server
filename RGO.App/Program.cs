4280749df75de7d07c64ced17dabbbd9b970add9
ksabjee <ksabjee@retrorabbit.co.za>
2023-08-21 12:03:13 +0200

    update

--- RGO.App/Program.cs
+++ RGO.App/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using RGO.Services;
