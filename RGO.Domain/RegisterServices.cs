4c4efb8df9b08d2b40206d3fda299c1fadaef0aa
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-08-21 17:01:53 +0200

    Registering service

--- RGO.Domain/RegisterServices.cs
+++ RGO.Domain/RegisterServices.cs
@@ -1,20 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using RGO.Services.Interfaces;
 using RGO.Services.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace RGO.Services
+namespace RGO.Services;
+
+public static class RegisterServicesExtension
 {
-    public static class RegisterServicesExtension
+    public static void RegisterServices(this IServiceCollection services)
     {
-        public static void RegisterServices(this IServiceCollection services)
-        {
-            services.AddScoped<IAuthService, AuthService>();
-            services.AddScoped<IEmployeeService, EmployeeService>();
-        }
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IEmployeeService, EmployeeService>();
+        services.AddScoped<IEmployeeTypeService, EmployeeTypeService>();
+        services.AddScoped<IOnboardingDocumentService, OnboardingDocumentService>();
+        services.AddScoped<IRoleAccessService, RoleAccessService>();
+        services.AddScoped<IRoleService, RoleService>();
     }
 }
