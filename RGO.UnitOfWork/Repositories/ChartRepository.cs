8280dd915e37e79e7cdffb0c20e1d211a15fd842
DewanAlberts <dalberts@retrorabbit.co.za>
2023-08-24 17:10:00 +0200

    Interfaces and Repository setup of Chart and ChartRolelink

---
+++ RGO.UnitOfWork/Repositories/ChartRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RGO.Models;
+using RGO.UnitOfWork.Entities;
+using RGO.UnitOfWork.Interfaces;
+
+namespace RGO.UnitOfWork.Repositories
+{
+    public class ChartRepository : BaseRepository<Chart,ChartDto>, IChartRepository
+    {
+        public ChartRepository(DatabaseContext db) : base(db)
+        {
+        }
+    }
+}
