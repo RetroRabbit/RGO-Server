09207bd4f36c3db7d1beb97e55a1b23b6b2c08b9
VhulendaNdou <vndou@retrorabbit.co.za>
2023-08-25 08:45:23 +0200

    Added the controller and service

---
+++ RGO.App/Controllers/ChartsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RGO.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartsController : ControllerBase
+    {
+    }
+}
