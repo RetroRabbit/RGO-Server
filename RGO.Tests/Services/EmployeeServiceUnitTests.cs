e788917ec6eda5f4c1a78e52dddee1761f82190f
Dorothy Mahoko <mahokod@fourier.co.za>
2023-08-28 11:28:49 +0200

    Employee Service Save test

--- RGO.Tests/Services/EmployeeServiceUnitTests.cs
+++ RGO.Tests/Services/EmployeeServiceUnitTests.cs
@@ -12,8 +12,9 @@ using RGO.UnitOfWork;
 using RGO.Models;
 using RGO.UnitOfWork.Interfaces;
 using RGO.UnitOfWork.Entities;
+using Microsoft.OpenApi.Any;
 
-namespace RGO.Tests
+namespace RGO.Tests.Services
 {
     public class EmployeeServiceUnitTests
     {
@@ -24,24 +25,26 @@ namespace RGO.Tests
             var _dbMock = new Mock<IUnitOfWork>();
             var employeeRepositoryMock = new Mock<IEmployeeRepository>();
 
-            EmployeeDto employeeDto = new EmployeeDto(1, "001", "34434434", new DateTime(), new DateTime(),
-                null, false, "None", 4, null, "Notes", 1, 28, 128, 100000, "Ms", "Dorothy", "D",
-                "Mahoko", new DateTime(), "South Africa", "South African", "9708180344086", " ",
-                new DateTime(), null, Models.Enums.Race.Black, Models.Enums.Gender.Female, null, 
-                "dmahoko@retrorabbit.co.za", "dimphomahoko@gmail.com", "0848645127");
 
             EmployeeTypeDto employeeTypeDto = new EmployeeTypeDto(1, "Developer");
+            EmployeeType employeeType = new EmployeeType(employeeTypeDto);
 
-            Employee employee = new Employee(employeeDto, employeeTypeDto);
+            employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
 
-            _dbMock.Setup(r => r.Employee.Add(employee)).Returns(Task.FromResult(employeeDto));
+            EmployeeDto employeeDto = new EmployeeDto(1, "001", "34434434", new DateTime(), new DateTime(),
+                null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Ms", "Dorothy", "D",
+                "Mahoko", new DateTime(), "South Africa", "South African", "9708180344086", " ",
+                new DateTime(), null, Models.Enums.Race.Black, Models.Enums.Gender.Female, null,
+                "dmahoko@retrorabbit.co.za", "dimphomahoko@gmail.com", "0848645127");
+
+            _dbMock.Setup(r => r.Employee.Add(It.IsAny<Employee>())).Returns(Task.FromResult(employeeDto));
 
             var employeeService = new EmployeeService(employeeTypeServiceMock.Object, _dbMock.Object);
 
             var result = await employeeService.SaveEmployee(employeeDto);
             Assert.NotNull(result);
+            Assert.Equal(employeeDto, result);
         }
-
 
     }
 }
