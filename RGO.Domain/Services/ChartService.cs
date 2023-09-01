03d44f94a5ad0de0685dca91c1dad3130cdd9b5a
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-09-01 14:47:21 +0200

    #5855-Created get chart data method and refactored the code

--- RGO.Domain/Services/ChartService.cs
+++ RGO.Domain/Services/ChartService.cs
@@ -35,55 +35,92 @@ public class ChartService : IChartService
         }
     }
 
-    public async Task<ChartDto> CreateChart(string dataType,string chartName,string chartType)
+    public async Task<ChartDto> CreateChart(string dataType, string chartName, string chartType)
     {
-        if (dataType == "Gender")
+        var employees = await _employeeService.GetAll();
+
+        Dictionary<string, int> dataDictionary = null;
+
+        switch (dataType)
         {
-            var employess = await _employeeService.GetAll();
-            var genderCounts = employess.GroupBy(x => x.Gender).ToList();
-
-            var labels = genderCounts.Select(group => group.Key.ToString()).ToList();
-            var data = genderCounts.Select(group => group.Count()).ToList();
-
-            var chart = new Chart
-            { 
-                Name= chartName,
-                Type= chartType,
-                Labels= labels,
-                Data= data
-            };
-
-            return await _db.Chart.Add(chart);
+            case "Gender":
+                dataDictionary = employees
+                    .GroupBy(x => x.Gender)
+                    .ToDictionary(group => group.Key.ToString(), group => group.Count());
+                break;
+            case "Race":
+                dataDictionary = employees
+                    .GroupBy(x => x.Race)
+                    .ToDictionary(group => group.Key.ToString(), group => group.Count());
+                break;
+            case "Level":
+                dataDictionary = employees
+                    .GroupBy(x => x.Level)
+                    .ToDictionary(group => group.Key.ToString(), group => group.Count());
+                break;
         }
-        else
+
+        if (dataDictionary == null)
         {
             return null;
         }
+
+        var labels = dataDictionary.Keys.ToList();
+        var data = dataDictionary.Values.ToList();
+
+        var chart = new Chart
+        {
+            Name = chartName,
+            Type = chartType,
+            Labels = labels,
+            Data = data
+        };
+
+        return await _db.Chart.Add(chart);
     }
 
     public async Task<ChartDataDto> GetChartData(string dataType)
     {
-        if (dataType == "Gender")
+        var employees = await _employeeService.GetAll();
+
+        Dictionary<string, int> chartData = null;
+
+        switch (dataType)
         {
-            var employess = await _employeeService.GetAll();
-            var genderCounts = employess.GroupBy(x => x.Gender).ToList();
-
-            var labels = genderCounts.Select(group => group.Key.ToString()).ToList();
-            var data = genderCounts.Select(group => group.Count()).ToList();
-
-            var chartData = new ChartDataDto
-            {
-                Labels =labels,
-                Data = data
-            };
-
-            return chartData;
+            case "Gender":
+                chartData = employees
+                    .GroupBy(x => x.Gender)
+                    .ToDictionary(group => group.Key.ToString(), group => group.Count());
+                break;
+            case "Race":
+                chartData = employees
+                    .GroupBy(x => x.Race)
+                    .ToDictionary(group => group.Key.ToString(), group => group.Count());
+                break;
+            case "Level":
+                chartData = employees
+                    .GroupBy(x => x.Level)
+                    .ToDictionary(group => group.Key.ToString(), group => group.Count());
+                break;
+            default:
+                return null;
         }
-        else
+
+        if (chartData == null)
         {
             return null;
         }
-    }
 
+        var labels = chartData.Keys.ToList();
+        var data = chartData.Values.ToList();
+
+        var chartDataDto = new ChartDataDto
+        {
+            Labels = labels,
+            Data = data
+        };
+
+        return chartDataDto;
+    }
 
 }
