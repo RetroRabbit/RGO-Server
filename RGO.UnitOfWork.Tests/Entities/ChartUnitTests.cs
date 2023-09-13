12d2dc77ae44f0e8da8bb10e4fc2e817f8842b93
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-09-13 15:27:24 +0200

    #6000-Added unit testing

---
+++ RGO.UnitOfWork.Tests/Entities/ChartUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using RGO.UnitOfWork.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RGO.UnitOfWork.Tests.Entities
+{
+    public class ChartUnitTests
+    {
+        [Fact]
+        public async Task ChartTest()
+        {
+            var chart = new Chart();
+            Assert.IsType<Chart>(chart);
+            Assert.NotNull(chart);
+        }
+      
+    }
+}
