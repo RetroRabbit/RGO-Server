d7f95a67299015a045a4d43b3e5092b5800b9d83
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-09-18 09:59:06 +0200

    Update EmployeeEvaluationService.cs
    Update EmployeeEvaluation Service with crud methods.

--- RGO.Domain/Services/EmployeeEvaluationService.cs
+++ RGO.Domain/Services/EmployeeEvaluationService.cs
@@ -1,41 +1,69 @@
-﻿using RGO.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using RGO.Models;
 using RGO.Services.Interfaces;
 using RGO.UnitOfWork;
 using RGO.UnitOfWork.Entities;
 
-namespace RGO.Services.Services;
-
-public class EmployeeEvaluationService : IEmployeeEvaluationService
+namespace RGO.Services.Services
 {
-    private readonly IUnitOfWork _db;
-    public EmployeeEvaluationService(IUnitOfWork db)
+    public class EmployeeEvaluationService : IEmployeeEvaluationService
     {
-        _db = db;
-    }
-    public async Task<EmployeeEvaluationDto> SaveEmployeeEvaluation(EmployeeEvaluationDto employeeEvaluationDto)
-    {
-        EmployeeEvaluationDto newEmployeeEvaluation = await _db.EmployeeEvaluation.Add(new EmployeeEvaluation(employeeEvaluationDto));
+        private readonly IUnitOfWork _db;
 
-        return newEmployeeEvaluation;
-    }
+        public EmployeeEvaluationService(IUnitOfWork db)
+        {
+            _db = db;
+        }
 
-    public Task<EmployeeEvaluationDto> DeleteEmployeeEvaluation(EmployeeEvaluationDto employeeEvaluationDto)
-    {
-        throw new NotImplementedException();
-    }
+        public async Task<EmployeeEvaluationDto> SaveEmployeeEvaluation(EmployeeEvaluationDto employeeEvaluationDto)
+        {
+            var existingEvaluation = await _db.EmployeeEvaluation
+                .Get(evaluation => evaluation.Id == employeeEvaluationDto.Id)
+                .FirstOrDefaultAsync();
 
-    public Task<List<EmployeeEvaluationDto>> GetAllEmployeeEvaluations()
-    {
-        throw new NotImplementedException();
-    }
+            if (existingEvaluation != null)
+            {
+                throw new InvalidOperationException("An evaluation with the given ID already exists.");
+            }
 
-    public Task<EmployeeEvaluationDto> GetEmployeeEvaluation(EmployeeEvaluationDto employeeEvaluationDto)
-    {
-        throw new NotImplementedException();
-    }
+            return await _db.EmployeeEvaluation.Add(new EmployeeEvaluation(employeeEvaluationDto));
+        }
 
-    public Task<EmployeeEvaluationDto> UpdateEmployeeEvaluation(EmployeeEvaluationDto employeeEvaluationDto)
-    {
-        throw new NotImplementedException();
+        public async Task<EmployeeEvaluationDto> DeleteEmployeeEvaluation(EmployeeEvaluationDto employeeEvaluationDto)
+        {
+            var existingEvaluation = await GetEmployeeEvaluation(employeeEvaluationDto);
+
+            if (existingEvaluation == null)
+            {
+                throw new InvalidOperationException("No evaluation found with the given ID to delete.");
+            }
+
+            return await _db.EmployeeEvaluation.Delete(existingEvaluation.Id);
+        }
+
+        public async Task<EmployeeEvaluationDto> GetEmployeeEvaluation(EmployeeEvaluationDto evaluationDto)
+        {
+            return await _db.EmployeeEvaluation
+                .Get(evaluation => evaluation.Id == evaluationDto.Id)
+                .Select(evaluation => evaluation.ToDto())
+                .FirstOrDefaultAsync() ?? throw new InvalidOperationException("No evaluation found with the given ID.");
+        }
+
+        public async Task<EmployeeEvaluationDto> UpdateEmployeeEvaluation(EmployeeEvaluationDto employeeEvaluationDto)
+        {
+            var existingEvaluation = await GetEmployeeEvaluation(employeeEvaluationDto);
+
+            if (existingEvaluation == null)
+            {
+                throw new InvalidOperationException("No evaluation found with the given ID to update.");
+            }
+
+            return await _db.EmployeeEvaluation.Update(new EmployeeEvaluation(existingEvaluation));
+        }
+
+        public Task<List<EmployeeEvaluationDto>> GetAllEmployeeEvaluations()
+        {
+            return _db.EmployeeEvaluation.GetAll();
+        }
     }
 }
