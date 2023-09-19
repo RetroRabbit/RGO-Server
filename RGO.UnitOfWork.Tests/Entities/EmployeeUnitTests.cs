b5b3d973d45592880c183d39b7457382bd67301d
Dorothy Mahoko <mahokod@fourier.co.za>
2023-09-19 09:17:38 +0200

    employee

---
+++ RGO.UnitOfWork.Tests/Entities/EmployeeUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using RGO.UnitOfWork.Entities;
+using Xunit;
+
+namespace RGO.UnitOfWork.Tests.Entities
+{
+    public class EmployeeUnitTests
+    {
+        [Fact]
+        public async Task EmployeeTest()
+        {
+            var employee = new Employee();
+            Assert.IsType<Employee>(employee);
+            Assert.NotNull(employee);
+        }
+    }
+}
