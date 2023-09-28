3d7f197ff44f6d4b42cd4cd2c024f33ec1765402
Kamogelo Matsomela <kmatsomela@retrorabbit.co.za>
2023-09-28 09:59:22 +0200

    Updating returns

--- RGO.Domain/Interfaces/IEmployeeEvaluationAudienceService.cs
+++ RGO.Domain/Interfaces/IEmployeeEvaluationAudienceService.cs
@@ -9,7 +9,7 @@ public interface IEmployeeEvaluationAudienceService
     /// </summary>
     /// <param name="evaluation"></param>
     /// <param name="email"></param>
-    /// <returns></returns>
+    /// <returns>true or False</returns>
     Task<bool> CheckIfExists(EmployeeEvaluationDto evaluation, string email);
 
     /// <summary>
@@ -17,7 +17,7 @@ public interface IEmployeeEvaluationAudienceService
     /// </summary>
     /// <param name="evaluation"></param>
     /// <param name="email"></param>
-    /// <returns></returns>
+    /// <returns>Employee Evaluation Audience</returns>
     Task<EmployeeEvaluationAudienceDto> Get(EmployeeEvaluationDto evaluation, string email);
 
     /// <summary>
@@ -25,7 +25,7 @@ public interface IEmployeeEvaluationAudienceService
     /// </summary>
     /// <param name="email"></param>
     /// <param name="evaluationInput"></param>
-    /// <returns></returns>
+    /// <returns>Employee Evaluation Audience</returns>
     Task<EmployeeEvaluationAudienceDto> Save(string email, EmployeeEvaluationInput evaluationInput);
 
     /// <summary>
@@ -33,33 +33,33 @@ public interface IEmployeeEvaluationAudienceService
     /// </summary>
     /// <param name="evaluation"></param>
     /// <param name="email"></param>
-    /// <returns></returns>
+    /// <returns>Employee Evaluation Audience</returns>
     Task<EmployeeEvaluationAudienceDto> Delete(string email, EmployeeEvaluationInput evaluationInput);
 
     /// <summary>
     /// Update Employee Evaluation Audience
     /// </summary>
     /// <param name="employeeEvaluationAudienceDto"></param>
-    /// <returns></returns>
+    /// <returns>Employee Evaluation Audience</returns>
     Task<EmployeeEvaluationAudienceDto> Update(EmployeeEvaluationAudienceDto employeeEvaluationAudienceDto);
 
     /// <summary>
     /// Get all Employee Evaluation Audiences
     /// </summary>
-    /// <returns></returns>
+    /// <returns>List of Employee Evaluation Audience</returns>
     Task<List<EmployeeEvaluationAudienceDto>> GetAll();
 
     /// <summary>
     /// Get all Employee Evaluation Audiences by Evaluation
     /// </summary>
     /// <param name="evaluation"></param>
-    /// <returns></returns>
+    /// <returns>List of Employee Evaluation Audience</returns>
     Task<List<EmployeeEvaluationAudienceDto>> GetAllbyEvaluation(EmployeeEvaluationInput evaluation);
 
     /// <summary>
     /// Get all Employee Evaluation Audiences by Employee
     /// </summary>
     /// <param name="email"></param>
-    /// <returns></returns>
+    /// <returns>List of Employee Evaluation Audience</returns>
     Task<List<EmployeeEvaluationAudienceDto>> GetAllbyEmployee(string email);
 }
