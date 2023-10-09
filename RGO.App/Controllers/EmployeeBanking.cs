b1860b19857e2cf1b355757929b5d39bcf3e3f1b
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-10-09 11:48:22 +0200

    Created employeeBanking controller

---
+++ RGO.App/Controllers/EmployeeBanking.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RGO.App.Controllers;
+
+public class EmployeeBanking : ControllerBase
+{
+    [Authorize(Policy = "AdminOrSuperAdminPolicy")]
+    [HttpGet("getPending")]
+    public async Task<IActionResult> FetchPending()
+    {
+        return Ok("Your controller works");
+    }
+}
