df59c518b4e8586447a990bbed19a2bb31c45d5c
Kamogelo Matsomela <kmatsomela@retrorabbit.co.za>
2023-10-11 22:17:44 +0200

    Adding EmployeeDate service

---
+++ RGO.Services/Interfaces/IEmployeeDateService.cs
@@ -0,0 +1,16 @@
+﻿using RGO.Models;
+
+namespace RGO.Services.Interfaces;
+
+public interface IEmployeeDateService
+{
+    Task<bool> CheckIfExists(EmployeeDateDto employeeDate);
+    Task Save(EmployeeDateDto employeeDate);
+    Task Delete(EmployeeDateDto employeeDate);
+    Task Update(EmployeeDateDto newEmployeeDate);
+    Task<EmployeeDateDto> Get(EmployeeDateDto employeeDate);
+    List<EmployeeDateDto> GetAll();
+    List<EmployeeDateDto> GetAllByEmployee(string email);
+    List<EmployeeDateDto> GetAllBySubject(string subject);
+    List<EmployeeDateDto> GetAllByDate(DateOnly Date);
+}
