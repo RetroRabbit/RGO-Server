e8d0e45a62ae210e8fac33e00514e25753284c7b
Kamogelo Matsomela <kmatsomela@retrorabbit.co.za>
2023-10-11 22:18:53 +0200

    Adding EmployeeDate entity unit tests

--- RGO.UnitOfWork.Tests/Entities/EmployeeDateUnitTests.cs
+++ RGO.UnitOfWork.Tests/Entities/EmployeeDateUnitTests.cs
@@ -1,64 +1,60 @@
 ﻿using RGO.Models;
 using RGO.UnitOfWork.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
-namespace RGO.UnitOfWork.Tests.Entities
+namespace RGO.UnitOfWork.Tests.Entities;
+
+public class EmployeeDateUnitTests
 {
-    public class EmployeeDateUnitTests
+    private readonly EmployeeDto _employee;
+
+    public EmployeeDateUnitTests()
     {
-        private EmployeeDto _employee;
+        EmployeeTypeDto employeeTypeDto = new(1, "Developer");
 
-        public EmployeeDateUnitTests() 
+        _employee = new EmployeeDto(1, "001", "34434434", new DateOnly(), new DateOnly(),
+            null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Ms", "Dorothy", "D",
+            "Mahoko", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
+            new DateOnly(), null, Models.Enums.Race.Black, Models.Enums.Gender.Male, null!,
+            "texample@retrorabbit.co.za", "test.example@gmail.com", "0000000000");
+    }
+
+    public EmployeeDate CreateEmployeeDate(EmployeeDto? employee = null)
+    {
+        EmployeeDate employeeDate = new()
         {
-            EmployeeTypeDto employeeTypeDto = new EmployeeTypeDto(1, "Developer");
+            Id = 1,
+            EmployeeId = 1,
+            Subject = "",
+            Note = "",
+            Date = DateOnly.FromDateTime(DateTime.Now),
+        };
 
-            _employee = new EmployeeDto(1, "001", "34434434", new DateOnly(), new DateOnly(),
-                null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Ms", "Dorothy", "D",
-                "Mahoko", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
-                new DateOnly(), null, Models.Enums.Race.Black, Models.Enums.Gender.Male, null,
-                "texample@retrorabbit.co.za", "test.example@gmail.com", "0000000000");
-        }
-
-        public EmployeeDate CreateEmployeeDate(EmployeeDto employee)
-        {
-            EmployeeDate employeeDate = new EmployeeDate
-            {
-                Id = 1,
-                EmployeeId = 1,
-                Code = "",
-                Date = DateTime.Now
-            };
-
+        if (employee != null)
             employeeDate.Employee = new Employee(employee, employee.EmployeeType);
 
-            return employeeDate;
-        }
+        return employeeDate;
+    }
 
-        [Fact]
-        public void EmployeeDateTest()
-        {
-            var employeeDate = new EmployeeDate();
-            Assert.IsType<EmployeeDate>(employeeDate);
-            Assert.NotNull(employeeDate);
-        }
+    [Fact]
+    public void EmployeeDateTest()
+    {
+        var employeeDate = new EmployeeDate();
+        Assert.IsType<EmployeeDate>(employeeDate);
+        Assert.NotNull(employeeDate);
+    }
 
-        [Fact]
-        public void EmployeeDateToDTO()
-        {
-            var employeeDate = CreateEmployeeDate(employee: _employee);
-            var employeeDateDto = employeeDate.ToDto();
+    [Fact]
+    public void EmployeeDateToDTO()
+    {
+        var employeeDate = CreateEmployeeDate(employee: _employee);
+        var employeeDateDto = employeeDate.ToDto();
 
-            Assert.NotNull(employeeDateDto.Employee);
-            Assert.Equal(employeeDate.EmployeeId, employeeDateDto.Employee!.Id);
-            Assert.IsType<EmployeeDateDto>(employeeDateDto);
+        Assert.NotNull(employeeDateDto.Employee);
+        Assert.Equal(employeeDate.EmployeeId, employeeDateDto.Employee!.Id);
+        Assert.IsType<EmployeeDateDto>(employeeDateDto);
 
-            var initializedEmployeeDate = new EmployeeDate(employeeDateDto);
-            Assert.Null(initializedEmployeeDate.Employee);
-        }
+        var initializedEmployeeDate = new EmployeeDate(employeeDateDto);
+        Assert.Null(initializedEmployeeDate.Employee);
     }
 }
