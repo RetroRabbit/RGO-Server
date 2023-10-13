63d6c7d82dbab487bccb828437b454c4619e79e3
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-10-13 16:17:16 +0200

    Fixed Update Unit Test

--- RGO.Services.Tests/Services/EmployeeBankingServiceUnitTest.cs
+++ RGO.Services.Tests/Services/EmployeeBankingServiceUnitTest.cs
@@ -12,10 +12,12 @@ using System.Linq.Expressions;
 using RGO.Services.Interfaces;
 using MockQueryable.Moq;
 
+
 public class EmployeeBankingServiceTest
 {
     private readonly Mock<IUnitOfWork> _mockUnitOfWork;
     private readonly Mock<IEmployeeBankingService> _employeeBankingService;
+    private readonly Mock<IEmployeeTypeService> employeeTypeServiceMock;
     private EmployeeBankingService employeeBankingService;
 
     public EmployeeBankingServiceTest()
@@ -23,6 +25,7 @@ public class EmployeeBankingServiceTest
         _mockUnitOfWork = new Mock<IUnitOfWork>();
         _employeeBankingService = new Mock<IEmployeeBankingService>();
         employeeBankingService = new EmployeeBankingService(_mockUnitOfWork.Object);
+        employeeTypeServiceMock = new Mock<IEmployeeTypeService>();
     }
 
     [Fact]
@@ -64,50 +67,34 @@ public class EmployeeBankingServiceTest
         Assert.Single(result);
     }
 
-   /* [Fact]
+    [Fact]
     public async Task UpdateReturnsUpdateBankDtos()
     {
-
-        var employeeTypeServiceMock = new Mock<IEmployeeTypeService>();
-
         EmployeeBankingDto test1 = new EmployeeBankingDto(
             1, 1, "FNB", "Not Sure", "120", EmployeeBankingAccountType.Savings, "Name1", BankApprovalStatus.PendingApproval, "", "asd");
-
-        var pendingBankEntries = new List<EmployeeBankingDto> { test1 };
 
         EmployeeTypeDto employeeTypeDto = new EmployeeTypeDto(1, "Developer");
         EmployeeType employeeType = new EmployeeType(employeeTypeDto);
 
+        employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
+        
         EmployeeDto testEmployee = new(1, "001", "34434434", new DateOnly(), new DateOnly(),
         null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Mr", "Matt", "MT",
         "Schoeman", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
         new DateOnly(), null, Race.Black, Gender.Male, null,
         "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000");
 
-        Assert.NotNull(testEmployee);
-        Assert.NotNull(employeeTypeDto);
+        Employee employee = new Employee(testEmployee, testEmployee.EmployeeType);
+        employee.EmployeeType = employeeType;
+        List<Employee> employees = new List<Employee>();
+        employees.Add(employee);
+        var mockEmployees = employees;
 
-        var employees = new List<Employee>
-        {
-            new Employee(testEmployee, employeeTypeDto)
-        };
-
-        var empType = new List<EmployeeType>
-        {
-            new EmployeeType(employeeTypeDto)
-        };
-
-        var mockEmployees = employees.AsQueryable().BuildMock();
-
-        employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeTypeDto.Name)).Returns(Task.FromResult(employeeTypeDto));
-
-        _mockUnitOfWork.Setup(u => u.Employee.Get(It.IsAny<Expression<Func<Employee, bool>>>())).Returns(mockEmployees);
+        _mockUnitOfWork.Setup(u => u.Employee.Get(It.IsAny<Expression<Func<Employee, bool>>>())).Returns(mockEmployees.AsQueryable().BuildMock());
         _mockUnitOfWork.Setup(u => u.EmployeeBanking.Update(It.IsAny<EmployeeBanking>()));
-       // _mockUnitOfWork.Setup(u => u.EmployeeType.Any(It.IsAny<Expression<Func<EmployeeType, bool>>>())).Returns();
-       // _mockUnitOfWork.Setup(u => u)
         var result = await employeeBankingService.UpdatePending(test1);
 
         Assert.Equal(test1, result);
-    }*/
+    }
 }
 
