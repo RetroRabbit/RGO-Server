c17b4525f81db286ea763f2cc9c5207dcfd12752
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-11-15 12:13:49 +0200

    Update FieldCodeControllerUnitTests.cs
    Added GetByCategory unit tests.

--- RGO.App.Tests/Controllers/FieldCodeControllerUnitTests.cs
+++ RGO.App.Tests/Controllers/FieldCodeControllerUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 using Moq;
 using RGO.App.Controllers;
 using RGO.Models;
@@ -22,12 +23,12 @@ namespace RGO.App.Tests.Controllers
         [Fact]
         public async Task GetAllFieldCodes_ReturnsOkResult_WithListOfFieldCodes()
         {
-            var fieldCodes = new List<FieldCodeDto> 
+            var fieldCodes = new List<FieldCodeDto>
             {
-                new FieldCodeDto(1,"Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1"),
-                new FieldCodeDto(2, "Code2", "Name2", "Description2", "Regex2", FieldCodeType.String, ItemStatus.Active, true, "InternalTable2")
+                new FieldCodeDto(1,"Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", 0),
+                new FieldCodeDto(2, "Code2", "Name2", "Description2", "Regex2", FieldCodeType.String, ItemStatus.Active, true, "InternalTable2", 0)
             };
-            
+
             _fieldCodeServiceMock.Setup(s => s.GetAllFieldCodes()).ReturnsAsync(fieldCodes);
 
             var result = await _controller.GetAllFieldCodes();
@@ -52,8 +53,8 @@ namespace RGO.App.Tests.Controllers
         [Fact]
         public async Task SaveFieldCode_ReturnsOkResult_WithSavedFieldCode()
         {
-            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1");
-            var savedFieldCode = new FieldCodeDto(2, "Code2", "Name2", "Description2", "Regex2", FieldCodeType.String, ItemStatus.Active, true, "InternalTable2");
+            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", 0);
+            var savedFieldCode = new FieldCodeDto(2, "Code2", "Name2", "Description2", "Regex2", FieldCodeType.String, ItemStatus.Active, true, "InternalTable2", 0);
             _fieldCodeServiceMock.Setup(s => s.SaveFieldCode(fieldCodeDto)).ReturnsAsync(savedFieldCode);
 
             var result = await _controller.SaveFieldCode(fieldCodeDto);
@@ -66,7 +67,7 @@ namespace RGO.App.Tests.Controllers
         [Fact]
         public async Task SaveFieldCode_ReturnsNotFoundResult_WhenExceptionThrown()
         {
-            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1"); ;
+            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", 0);
             var exceptionMessage = "An error occurred";
             _fieldCodeServiceMock.Setup(s => s.SaveFieldCode(fieldCodeDto)).ThrowsAsync(new Exception(exceptionMessage));
 
@@ -79,8 +80,8 @@ namespace RGO.App.Tests.Controllers
         [Fact]
         public async Task UpdateFieldCode_ReturnsOkResult_WithUpdatedFieldCode()
         {
-            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1");
-            var updatedFieldCode = new FieldCodeDto(2, "Code2", "Name2", "Description2", "Regex2", FieldCodeType.String, ItemStatus.Active, true, "InternalTable2");
+            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", 0);
+            var updatedFieldCode = new FieldCodeDto(2, "Code2", "Name2", "Description2", "Regex2", FieldCodeType.String, ItemStatus.Active, true, "InternalTable2", 0);
             _fieldCodeServiceMock.Setup(s => s.UpdateFieldCode(fieldCodeDto)).ReturnsAsync(updatedFieldCode);
 
             var result = await _controller.UpdateFieldCode(fieldCodeDto);
@@ -93,7 +94,7 @@ namespace RGO.App.Tests.Controllers
         [Fact]
         public async Task UpdateFieldCode_ReturnsNotFoundResult_WhenExceptionThrown()
         {
-            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1");
+            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", 0);
             var exceptionMessage = "An error occurred";
             _fieldCodeServiceMock.Setup(s => s.UpdateFieldCode(fieldCodeDto)).ThrowsAsync(new Exception(exceptionMessage));
 
@@ -106,8 +107,8 @@ namespace RGO.App.Tests.Controllers
         [Fact]
         public async Task DeleteFieldCode_ReturnsOkResult_WithDeletedFieldCode()
         {
-            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1");
-            var deletedFieldCode = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1");
+            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", 0);
+            var deletedFieldCode = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", 0);
             _fieldCodeServiceMock.Setup(s => s.DeleteFieldCode(fieldCodeDto)).ReturnsAsync(deletedFieldCode);
 
             var result = await _controller.DeleteFieldCode(fieldCodeDto);
@@ -120,7 +121,7 @@ namespace RGO.App.Tests.Controllers
         [Fact]
         public async Task DeleteFieldCode_ReturnsNotFoundResult_WhenExceptionThrown()
         {
-            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1");
+            var fieldCodeDto = new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", 0);
             var exceptionMessage = "An error occurred";
             _fieldCodeServiceMock.Setup(s => s.DeleteFieldCode(fieldCodeDto)).ThrowsAsync(new Exception(exceptionMessage));
 
@@ -128,6 +129,45 @@ namespace RGO.App.Tests.Controllers
 
             var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
             Assert.Equal(exceptionMessage, notFoundResult.Value);
+        }
+
+        [Fact]
+        public async Task GetByCategory_ValidCategory_ReturnsOkResultWithCategoryCodes()
+        {
+            var validCategory = 1;
+            var expectedCategoryCodes = new List<FieldCodeDto>
+            {
+                new FieldCodeDto(1, "Code1", "Name1", "Description1", "Regex1", FieldCodeType.String, ItemStatus.Active, true, "InternalTable1", FieldCodeCategory.Banking)
+            };
+
+            var mockFieldCodeService = new Mock<IFieldCodeService>();
+            mockFieldCodeService.Setup(service => service.GetByCategory(validCategory))
+                .ReturnsAsync(expectedCategoryCodes);
+
+            var controller = new FieldCodeController(mockFieldCodeService.Object);
+
+            var result = await controller.GetByCategory(validCategory);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualCategoryCodes = Assert.IsAssignableFrom<List<FieldCodeDto>>(okResult.Value);
+            Assert.Equal(expectedCategoryCodes, actualCategoryCodes);
+        }
+
+        [Fact]
+        public async Task GetByCategory_InvalidCategory_ReturnsNotFoundResult()
+        {
+            var invalidCategory = -1;
+
+            var mockFieldCodeService = new Mock<IFieldCodeService>();
+            mockFieldCodeService.Setup(s => s.GetByCategory(invalidCategory))
+                .ThrowsAsync(new Exception("Invalid Index"));
+
+            var controller = new FieldCodeController(mockFieldCodeService.Object);
+
+            var result = await controller.GetByCategory(invalidCategory);
+
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("Invalid Index", notFoundResult.Value);
         }
     }
 }
