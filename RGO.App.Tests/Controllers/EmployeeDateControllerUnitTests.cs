5cf821ade57ee43d238aed210131bc2b0d9dae1e
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-11-21 07:19:43 +0200

    Create EmployeeDateControllerUnitTests.cs

---
+++ RGO.App.Tests/Controllers/EmployeeDateControllerUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RGO.App.Controllers;
+using RGO.Models;
+using RGO.Models.Enums;
+using RGO.Services.Interfaces;
+using Xunit;
+
+namespace RGO.App.Tests.Controllers
+{
+    public class EmployeeDateControllerUnitTests
+    {
+        [Fact]
+        public async Task SaveEmployeeDateReturnsOkResult()
+        {
+
+        }
+    }
+}
