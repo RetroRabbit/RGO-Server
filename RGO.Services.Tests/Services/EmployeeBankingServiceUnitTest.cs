85a8ca90739443343cb1b54fe2772a603ac7b34f
ksabjee <ksabjee@retrorabbit.co.za>
2023-12-04 09:46:52 +0200

    Date only fields updated
    1. Employee DateOnly fields updated to DateTime
    2. TestDate changed based on the above change
    3. Unit test data type changed from dateonly to datetime
    4. New migration added

--- RGO.Services.Tests/Services/EmployeeBankingServiceUnitTest.cs
+++ RGO.Services.Tests/Services/EmployeeBankingServiceUnitTest.cs
@@ -44,22 +44,22 @@ public class EmployeeBankingServiceTest
         EmployeeType employeeType = new EmployeeType(employeeTypeDto);
         EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
 
-        EmployeeDto testEmployee1 = new(1, "001", "34434434", new DateOnly(), new DateOnly(),
+        EmployeeDto testEmployee1 = new(1, "001", "34434434", new DateTime(), new DateTime(),
         null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
-        "Schoeman", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
-        new DateOnly(), null, Race.Black, Gender.Male, null,
+        "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+        new DateTime(), null, Race.Black, Gender.Male, null,
         "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
 
-        EmployeeDto testEmployee2 = new(2, "001", "34434434", new DateOnly(), new DateOnly(),
+        EmployeeDto testEmployee2 = new(2, "001", "34434434", new DateTime(), new DateTime(),
         null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
-        "Schoeman", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
-        new DateOnly(), null, Race.Black, Gender.Male, null,
+        "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+        new DateTime(), null, Race.Black, Gender.Male, null,
         "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
 
-        EmployeeDto testEmployee3 = new(3, "001", "34434434", new DateOnly(), new DateOnly(),
+        EmployeeDto testEmployee3 = new(3, "001", "34434434", new DateTime(), new DateTime(),
         null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
-        "Schoeman", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
-        new DateOnly(), null, Race.Black, Gender.Male, null,
+        "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+        new DateTime(), null, Race.Black, Gender.Male, null,
         "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
 
         var employees = new List<Employee>
@@ -133,10 +133,10 @@ public class EmployeeBankingServiceTest
         employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
         EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
 
-        EmployeeDto testEmployee = new(1, "001", "34434434", new DateOnly(), new DateOnly(),
+        EmployeeDto testEmployee = new(1, "001", "34434434", new DateTime(), new DateTime(),
         null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
-        "Schoeman", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
-        new DateOnly(), null, Race.Black, Gender.Male, null,
+        "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+        new DateTime(), null, Race.Black, Gender.Male, null,
         "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
 
         Employee employee = new Employee(testEmployee, testEmployee.EmployeeType);
@@ -174,10 +174,10 @@ public class EmployeeBankingServiceTest
         employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
         EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
 
-        EmployeeDto testEmployee = new(1, "001", "34434434", new DateOnly(), new DateOnly(),
+        EmployeeDto testEmployee = new(1, "001", "34434434", new DateTime(), new DateTime(),
         null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
-        "Schoeman", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
-        new DateOnly(), null, Race.Black, Gender.Male, null,
+        "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+        new DateTime(), null, Race.Black, Gender.Male, null,
         "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
 
         Employee employee = new Employee(testEmployee, testEmployee.EmployeeType);
@@ -246,10 +246,10 @@ public class EmployeeBankingServiceTest
         employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
         EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
 
-        EmployeeDto testEmployee = new(1, "001", "34434434", new DateOnly(), new DateOnly(),
+        EmployeeDto testEmployee = new(1, "001", "34434434", new DateTime(), new DateTime(),
         null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
-        "Schoeman", new DateOnly(), "South Africa", "South African", "0000080000000", " ",
-        new DateOnly(), null, Race.Black, Gender.Male, null,
+        "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+        new DateTime(), null, Race.Black, Gender.Male, null,
         "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
 
         Employee employee = new Employee(testEmployee, testEmployee.EmployeeType);
