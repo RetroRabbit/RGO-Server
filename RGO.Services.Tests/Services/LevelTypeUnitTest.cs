c8fdf254cef2d125c3ec7d289d97503eacb77329
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2024-01-12 09:46:13 +0200

    Adding level type unit tests for LevelTypeService

---
+++ RGO.Services.Tests/Services/LevelTypeUnitTest.cs
@@ -0,0 +1,160 @@
+﻿using Moq;
+using Xunit;
+using RGO.Services.Services;
+using RGO.Services.Interfaces;
+using RGO.UnitOfWork;
+using RGO.Models;
+using RGO.UnitOfWork.Entities;
+using System.Linq.Expressions;
+using MockQueryable.Moq;
+using RGO.Models.Enums;
+
+namespace RGO.Services.Tests.Services;
+
+public class LevelTypeUnitTest
+{
+    private readonly Mock<IUnitOfWork> _dbMock;
+    private readonly Mock<IEmployeeTypeService> _employeeTypeServiceMock;
+    private LevelType levelType;
+    EmployeeTypeDto employeeTypeDto;
+    EmployeeType employeeType;
+    RoleDto roleDto;
+
+    public LevelTypeUnitTest()
+    {
+        _dbMock = new Mock<IUnitOfWork>();
+        _employeeTypeServiceMock = new Mock<IEmployeeTypeService>();
+        levelType = new LevelType();
+        employeeTypeDto = new EmployeeTypeDto(1, "Developer");
+        employeeType = new EmployeeType(employeeTypeDto);
+        roleDto = new RoleDto(3, "Employee");
+    }
+
+    [Fact]
+    public async Task LevelTypeNullTestSuccess()
+    {
+        _employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
+        EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
+
+        EmployeeDto employeeDto = new(1, "001", "34434434", new DateTime(), new DateTime(),
+            null, false, "None", null, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
+            "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+            new DateTime(), null, Race.Black, Gender.Male, null,
+            "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
+
+        var employeeDtoList = new List<EmployeeDto>
+        {
+            employeeDto
+        };
+        var employeeList = new List<Employee>
+        {
+            new Employee(employeeDto,employeeDto.EmployeeType)
+        };
+
+        _dbMock.Setup(e => e.Employee.Get(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .Returns(employeeList.AsQueryable().BuildMock());
+        
+        var realServiceProvider = Mock.Of<IServiceProvider>();
+        var result = levelType.GenerateData(employeeDto, realServiceProvider);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task LevelTypeNullFail()
+    {
+        _employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
+        EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
+        _employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeTypeDto.Name)).Throws(new Exception());
+
+        EmployeeDto employeeDto = new(1, "001", "34434434", new DateTime(), new DateTime(),
+            null, false, "None", null, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
+            "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+            new DateTime(), null, Race.Black, Gender.Male, null,
+            "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
+
+        var employeeDtoList = new List<EmployeeDto>
+        {
+            employeeDto
+        };
+        var employeeList = new List<Employee>
+        {
+            new Employee(employeeDto,employeeDto.EmployeeType)
+        };
+
+        _dbMock.Setup(r => r.EmployeeType.Any(It.IsAny<Expression<Func<EmployeeType, bool>>>())).Returns(Task.FromResult(false));
+        _dbMock.Setup(e => e.Employee.Get(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .Returns(employeeList.AsQueryable().BuildMock());
+
+        var realServiceProvider = Mock.Of<IServiceProvider>();
+        var result = levelType.GenerateData(employeeDto, realServiceProvider);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task LevelTypeValueTestSuccess()
+    {
+        _employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
+        EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
+        _employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeTypeDto.Name)).Throws(new Exception());
+
+        EmployeeDto employeeDto = new(1, "001", "34434434", new DateTime(), new DateTime(),
+            null, false, "None", 3, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
+            "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+            new DateTime(), null, Race.Black, Gender.Male, null,
+            "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
+
+        var employeeDtoList = new List<EmployeeDto>
+        {
+            employeeDto
+        };
+        var employeeList = new List<Employee>
+        {
+            new Employee(employeeDto,employeeDto.EmployeeType)
+        };
+
+        _dbMock.Setup(r => r.EmployeeType.Any(It.IsAny<Expression<Func<EmployeeType, bool>>>())).Returns(Task.FromResult(false));
+        _dbMock.Setup(r => r.Employee.Any(It.IsAny<Expression<Func<Employee, bool>>>())).Returns(Task.FromResult(false));
+        _dbMock.Setup(e => e.Employee.Get(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .Returns(employeeList.AsQueryable().BuildMock());
+
+        var realServiceProvider = Mock.Of<IServiceProvider>();
+        var result = levelType.GenerateData(employeeDto, realServiceProvider);
+
+        Assert.Equal("Level 3, ",result);
+    }
+
+    [Fact]
+    public async Task LevelTypeValueTestFail()
+    {
+        _employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeTypeDto.Name)).Throws(new Exception());
+        _employeeTypeServiceMock.Setup(r => r.GetEmployeeType(employeeType.Name)).Returns(Task.FromResult(employeeTypeDto));
+        EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
+
+        EmployeeDto employeeDto = new(1, "001", "34434434", new DateTime(), new DateTime(),
+            null, false, "None", 3, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Matt", "MT",
+            "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
+            new DateTime(), null, Race.Black, Gender.Male, null,
+            "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
+
+        var employeeDtoList = new List<EmployeeDto>
+        {
+            employeeDto
+        };
+        var employeeList = new List<Employee>
+        {
+            new Employee(employeeDto,employeeDto.EmployeeType)
+        };
+
+        _dbMock.Setup(r => r.EmployeeType.Any(It.IsAny<Expression<Func<EmployeeType, bool>>>())).Returns(Task.FromResult(false));
+        _dbMock.Setup(r => r.Employee.Any(It.IsAny<Expression<Func<Employee, bool>>>())).Returns(Task.FromResult(false));
+        _dbMock.Setup(e => e.Employee.Get(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .Returns(employeeList.AsQueryable().BuildMock());
+
+        var realServiceProvider = Mock.Of<IServiceProvider>();
+        var result = levelType.GenerateData(employeeDto, realServiceProvider);
+
+        Assert.Equal("Level 3, ",result);
+    }
+}
