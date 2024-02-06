647fabc113b9d63908ae74b51608416ee0202a49
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2024-02-06 14:12:54 +0200

    Fixed unit test: EmployeeSaveTest3

--- RGO.Tests.Data/Models/EmployeeTd.cs
+++ RGO.Tests.Data/Models/EmployeeTd.cs
@@ -22,4 +22,10 @@ public class EmployeeTd
         "Schoeman", new DateTime(), "South Africa", "South African", "0000080000000", " ",
         new DateTime(), null, Race.Black, Gender.Male, null,
         "test@retrorabbit.co.za", "test.example@gmail.com", "0000000000", null, null, EmployeeAddressTd.EmployeeAddressDto, EmployeeAddressTd.EmployeeAddressDto, null, null, null);
+
+    public static EmployeeDto EmployeeDto4 = new EmployeeDto(4, "001", "34434434", new DateTime(), new DateTime(),
+        2, false, "None", 4, EmployeeTypeTd.DeveloperType, "Notes", 1, 28, 128, 100000, "Dorothy", "D",
+        "Mahoko", new DateTime(), "South Africa", "South African", "5522522655", " ",
+        new DateTime(), null, Race.Black, Gender.Male, null,
+        "dm@retrorabbit.co.za", "dm@gmail.com", "0123456789", 1, 3, EmployeeAddressTd.EmployeeAddressDto, EmployeeAddressTd.EmployeeAddressDto, null, null, null);
 }
