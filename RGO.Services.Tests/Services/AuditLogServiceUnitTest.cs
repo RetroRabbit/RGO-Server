c8922842275683c4aa2cb4b2169f755358e97dc9
Mathilda Shongwe <mshongwe@retrorabbit.co.za>
2024-02-09 11:40:13 +0200

    Coverage for Audit Log Service Completed

--- RGO.Services.Tests/Services/AuditLogServiceUnitTest.cs
+++ RGO.Services.Tests/Services/AuditLogServiceUnitTest.cs
@@ -1,118 +1,140 @@
-﻿//using Moq;
-//using RGO.Models;
-//using RGO.Services.Interfaces;
-//using RGO.Services.Services;
-//using RGO.Tests.Data.Models;
-//using RGO.UnitOfWork;
-//using RGO.UnitOfWork.Entities;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Linq.Expressions;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using Microsoft.EntityFrameworkCore;
+using MockQueryable.Moq;
+using Moq;
+using Org.BouncyCastle.Security;
+using RGO.Models;
+using RGO.Services.Interfaces;
+using RGO.Services.Services;
+using RGO.Tests.Data.Models;
+using RGO.UnitOfWork;
+using RGO.UnitOfWork.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace RGO.Services.Tests.Services
-//{
-//    public class AuditLogServiceUnitTest
-//    {
-//        private readonly Mock<IUnitOfWork> _unitOfWork;
-//        private readonly AuditLogService _auditLogService;
-//        private readonly Mock<IServiceProvider> _services;
-//        private AuditLogService auditLogService;
-//        private readonly Mock<IAuditLogService> _auditLogServiceMock;
-//        private readonly AuditLogDto _auditLogDto;
-//        private readonly EmployeeDto _employee;
+namespace RGO.Services.Tests.Services
+{
+    public class AuditLogServiceUnitTest
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly AuditLogService _auditLogService;
+        private readonly AuditLogDto _auditLogDto;
+        private readonly EmployeeDto _employee;
+        public AuditLogServiceUnitTest()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _auditLogService = new AuditLogService(_unitOfWork.Object);
 
-//        public AuditLogServiceUnitTest()
-//        {
-//            _unitOfWork = new Mock<IUnitOfWork>();
-//            _auditLogServiceMock = new Mock<IAuditLogService>();
-//            _services = new Mock<IServiceProvider>();
-//            _auditLogService = new AuditLogService(_unitOfWork.Object);
 
-//            EmployeeDto? editFor = null;
-//            EmployeeDto? editBy = null;
+            EmployeeAddressDto employeeAddressDto = new EmployeeAddressDto(1, "2", "Complex", "2", "Suburb/District", "City", "Country", "Province", "1620");
+            EmployeeTypeDto employeeTypeDto = new(1, "Developer");
+            _employee= new EmployeeDto(1, "001", "34434434", new DateTime(), new DateTime(),
+               null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Kamo", "K.G.",
+               "Smith", new DateTime(), "South Africa", "South African", "1234457899", " ",
+               new DateTime(), null, Models.Enums.Race.Black, Models.Enums.Gender.Female, null!,
+               "ksmith@retrorabbit.co.za", "kmaosmith@gmail.com", "0123456789", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
+           
+            _auditLogDto = new AuditLogDto
+            {
+                Id = 1,
+                EditBy = _employee,
+                EditFor = _employee,
+                EditDate = new DateTime(),
+                Description = "Test Description"
+            };
+        }
 
-//            EmployeeTypeDto employeeTypeDto = new(1, "Developer");
-//            _employee = new EmployeeDto(1, "001", "34434434", new DateTime(), new DateTime(),
-//               null, false, "None", 4, employeeTypeDto, "Notes", 1, 28, 128, 100000, "Kamo", "K.G.",
-//               "Smith", new DateTime(), "South Africa", "South African", "1234457899", " ",
-//               new DateTime(), null, Models.Enums.Race.Black, Models.Enums.Gender.Female, null!,
-//               "ksmith@retrorabbit.co.za", "kmaosmith@gmail.com", "0123456789", null, null, employeeAddressDto, employeeAddressDto, null, null, null);
+        [Fact]
+        public async Task GetAllAuditLogTest()
+        {
+            List<AuditLogDto> auditlogs = new List<AuditLogDto>() { _auditLogDto };
 
-//            _auditLogDto = new AuditLogDto
-//            {
-//                Id = 0,
-//                EditFor = editFor,
-//                EditBy = editBy,
-//                EditDate = new DateTime(),
-//                Description = ""
-//            };
-//        }
+            _unitOfWork.Setup(a => a.AuditLog.GetAll(null)).Returns(Task.FromResult(auditlogs));
+            var result = await _auditLogService.GetAllAuditLogs();
 
-//        [Fact]
-//        public async Task GetAllAuditLogTest()
-//        {
-//            List<AuditLogDto> auditlogs = new List<AuditLogDto>() { _auditLogDto };
+            Assert.NotNull(result);
+            Assert.Single(result);
+            Assert.Equal(auditlogs, result);
 
-//            _unitOfWork.Setup(a => a.AuditLog.GetAll(null)).Returns(Task.FromResult(auditlogs));
-//            var result = await _auditLogService.GetAllAuditLogs();
+        }
 
-//            Assert.NotNull(result);
-//            Assert.Single(result);
-//            Assert.Equal(auditlogs, result);
+        [Fact]
+        public async Task GetAuditLogByEditedByIdTest()
+        {
+            var Id = 1;
 
-//        }
+            var auditloglistdto = new List<AuditLogDto> { _auditLogDto };
+           
+            _unitOfWork.Setup(x => x.AuditLog.GetAll(It.IsAny<Expression<Func<AuditLog, bool>>>())).Returns(Task.FromResult(auditloglistdto));
 
-//        [Fact]
-//        public async Task GetAuditLogByEditedByIdTest()
-//        {
+            var result = await _auditLogService.GetAuditLogByEditedById(Id);
 
-//        }
+            Assert.NotNull(result);
+            Assert.Equal(auditloglistdto, result);
+        }
 
-//        [Fact]
-//        public async Task SaveAuditLogsTest()
-//        {
-//            List<AuditLogDto> auditlog = new List<AuditLogDto>() { _auditLogDto };
-//            _unitOfWork.Setup(x => x.AuditLog.GetAll(It.IsAny<Expression<Func<AuditLog, bool>>>())).Returns(Task.FromResult(auditlog));
+        [Fact]
+        public async Task GetAuditLogByEditedForIdTest()
+        {
+            var Id = 1;
+            var auditloglistdto = new List<AuditLogDto> { _auditLogDto };
 
-//            _unitOfWork.Setup(x => x.AuditLog.Add(It.IsAny<AuditLog>())).Returns(Task.FromResult(_auditLogDto));
+            _unitOfWork.Setup(x => x.AuditLog.GetAll(It.IsAny<Expression<Func<AuditLog, bool>>>())).Returns(Task.FromResult(auditloglistdto));
 
-//            var result = await _auditLogService.SaveAuditLog(_auditLogDto);
+            var result = await _auditLogService.GetAuditLogByEditedForId(Id);
 
-//            Assert.NotNull(result);
-//            Assert.Equal(_auditLogDto, result);
-//            _unitOfWork.Verify(x => x.AuditLog.Add(It.IsAny<AuditLog>()));
-//        }
+            Assert.NotNull(result);
+            Assert.Equal(auditloglistdto, result);
+        }
 
-//        [Fact]
-//        public async Task UpdateAuditLogTest()
-//        {
+        [Fact]
+        public async Task SaveAuditLogsTest()
+        {
+            List<AuditLogDto> auditlog = new List<AuditLogDto>() { _auditLogDto };
 
-//        }
+            _unitOfWork.Setup(x => x.AuditLog.GetAll(It.IsAny<Expression<Func<AuditLog, bool>>>())).Returns(Task.FromResult(auditlog));
+            _unitOfWork.Setup(x => x.AuditLog.Add(It.IsAny<AuditLog>())).Returns(Task.FromResult(_auditLogDto));
 
-//        [Fact]
-//        public async Task DeleteAuditLogTest()
-//        {
-//            List<AuditLogDto> auditlog = new List<AuditLogDto>() { _auditLogDto };
-//            _unitOfWork.Setup(x => x.AuditLog.GetAll(null)).Returns(Task.FromResult(auditlog));
+            var result = await _auditLogService.SaveAuditLog(_auditLogDto);
 
-//            _unitOfWork.Setup(x => x.AuditLog.Delete(It.IsAny<int>()))
-//                .Returns(Task.FromResult(_auditLogDto));
+            Assert.NotNull(result);
+            Assert.Equal(_auditLogDto, result);
 
-//            var result = await _auditLogService.DeleteAuditLog(_auditLogDto);
-//            Assert.NotNull(result);
-//            Assert.Equal(_auditLogDto, result);
-//            _unitOfWork.Verify(r => r.AuditLog.Delete(It.IsAny<int>()), Times.Once);
-//        }
+            _unitOfWork.Verify(x => x.AuditLog.Add(It.IsAny<AuditLog>()));
+        }
 
-//        [Fact]
-//        public async Task CheckAuditLogTest()
-//        {
+        [Fact]
+        public async Task UpdateAuditLogTest()
+        {
+            _unitOfWork.SetupSequence(a => a.AuditLog.GetById(It.IsAny<int>())).ReturnsAsync(_auditLogDto);
+            _unitOfWork.Setup(a => a.AuditLog.Any(It.IsAny<Expression<Func<AuditLog, bool>>>())).ReturnsAsync(true);
+            _unitOfWork.Setup(a => a.AuditLog.Update(It.IsAny<AuditLog>())).Returns(Task.FromResult(_auditLogDto));
+           
+            var result = await _auditLogService.UpdateAuditLog(_auditLogDto);
 
-//        }
+            Assert.NotNull(result);
+            Assert.Equal(_auditLogDto, result);
 
-//    }
-//}
+            _unitOfWork.Verify(x => x.AuditLog.Update(It.IsAny<AuditLog>()));
+        }
+
+        [Fact]
+        public async Task DeleteAuditLogTest()
+        {
+            _unitOfWork.Setup(a => a.AuditLog.GetById(It.IsAny<int>())).ReturnsAsync(_auditLogDto);
+            _unitOfWork.Setup(x => x.AuditLog.Delete(It.IsAny<int>()))
+                .Returns(Task.FromResult(_auditLogDto));
+
+            var result = await _auditLogService.DeleteAuditLog(_auditLogDto);
+
+            Assert.NotNull(result);
+            Assert.Equal(_auditLogDto, result);
+
+            _unitOfWork.Verify(x => x.AuditLog.Delete(It.IsAny<int>()));
+        }
+    }
+}
