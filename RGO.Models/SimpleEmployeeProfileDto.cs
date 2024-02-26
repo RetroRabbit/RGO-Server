6eb77153f28bbf319ad25a02799049dc883acf2d
Juanro-Minne <jminne@retrorabbit.co.za>
2024-02-26 12:20:05 +0200

    fixed SimpleEmployeeProfile DTO

--- RGO.Models/SimpleEmployeeProfileDto.cs
+++ RGO.Models/SimpleEmployeeProfileDto.cs
@@ -2,46 +2,47 @@
 
 namespace HRIS.Models;
 
-public record SimpleEmployeeProfileDto(
-    int Id,
-    string? EmployeeNumber,
-    string? TaxNumber,
-    DateTime EngagementDate,
-    DateTime? TerminationDate,
-    string? PeopleChampionName,
-    int? PeopleChampionId,
-    bool Disability,
-    string DisabilityNotes,
-    int? Level,
-    EmployeeTypeDto? EmployeeType,
-    string? Notes,
-    float? LeaveInterval,
-    float? SalaryDays,
-    float? PayRate,
-    int? Salary,
-    string Name,
-    string Initials,
-    string Surname,
-    DateTime DateOfBirth,
-    string? CountryOfBirth,
-    string? Nationality,
-    string IdNumber,
-    string? PassportNumber,
-    DateTime? PassportExpirationDate,
-    string? PassportCountryIssue,
-    Race? Race,
-    Gender? Gender,
-    string? Photo,
-    string Email,
-    string PersonalEmail,
-    string CellphoneNo,
-    string? ClientAllocatedName,
-    int? ClientAllocatedId,
-    string? TeamLeadName,
-    int? TeamLeadId,
-    EmployeeAddressDto? PhysicalAddress,
-    EmployeeAddressDto? PostalAddress,
-    string? HouseNo,
-    string? EmergencyContactName,
-    string? EmergencyContactNo
-);
+public class SimpleEmployeeProfileDto
+{
+    public int Id { get; set; }
+    public string? EmployeeNumber { get; set; }
+    public string? TaxNumber { get; set; }
+    public DateTime EngagementDate { get; set; }
+    public DateTime? TerminationDate { get; set; }
+    public string? PeopleChampionName { get; set; }
+    public int? PeopleChampionId { get; set; }
+    public bool Disability { get; set; }
+    public string DisabilityNotes { get; set; }
+    public int? Level { get; set; }
+    public EmployeeTypeDto? EmployeeType { get; set; }
+    public string? Notes { get; set; }
+    public float? LeaveInterval { get; set; }
+    public float? SalaryDays { get; set; }
+    public float? PayRate { get; set; }
+    public int? Salary { get; set; }
+    public string Name { get; set; }
+    public string Initials { get; set; }
+    public string Surname { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string? CountryOfBirth { get; set; }
+    public string? Nationality { get; set; }
+    public string IdNumber { get; set; }
+    public string? PassportNumber { get; set; }
+    public DateTime? PassportExpirationDate { get; set; }
+    public string? PassportCountryIssue { get; set; }
+    public Race? Race { get; set; }
+    public Gender? Gender { get; set; }
+    public string? Photo { get; set; }
+    public string Email { get; set; }
+    public string PersonalEmail { get; set; }
+    public string CellphoneNo { get; set; }
+    public string? ClientAllocatedName { get; set; }
+    public int? ClientAllocatedId { get; set; }
+    public string? TeamLeadName { get; set; }
+    public int? TeamLeadId { get; set; }
+    public EmployeeAddressDto? PhysicalAddress { get; set; }
+    public EmployeeAddressDto? PostalAddress { get; set; }
+    public string? HouseNo { get; set; }
+    public string? EmergencyContactName { get; set; }
+    public string? EmergencyContactNo { get; set; }
+}
