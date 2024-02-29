719fd2237b7e07f76eb830b3b2c2ecb92d0a8950
Juanro-Minne <jminne@retrorabbit.co.za>
2024-02-29 11:39:05 +0200

    EmployeeProjectDto refactor

--- HRIS.Models/EmployeeProjectDto.cs
+++ HRIS.Models/EmployeeProjectDto.cs
@@ -2,24 +2,6 @@
 
 public class EmployeeProjectDto
 {
-    // TODO : Remove this Constructor + Update UNIT TESTS
-    public EmployeeProjectDto(int Id,
-                              int EmployeeId,
-                              string Name,
-                              string Description,
-                              string Client,
-                              DateTime StartDate,
-                              DateTime? EndDate)
-    {
-        this.Id = Id;
-        this.EmployeeId = EmployeeId;
-        this.Name = Name;
-        this.Description = Description;
-        this.Client = Client;
-        this.StartDate = StartDate;
-        this.EndDate = EndDate;
-    }
-
     public int Id { get; set; }
     public int EmployeeId { get; set; }
     public string Name { get; set; }
