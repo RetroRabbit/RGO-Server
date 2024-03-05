43ca4014ff0312bb647a31cc00fba064b83f57ae
KayembaJonathan <kayembejonathan5@gmail.com>
2024-03-05 13:45:02 +0200

    Update EmployeeEvaluationTemplateItemTestData.cs

--- RR.Tests.Data/Models/HRIS/EmployeeEvaluationTemplateItemTestData.cs
+++ RR.Tests.Data/Models/HRIS/EmployeeEvaluationTemplateItemTestData.cs
@@ -15,3 +15,15 @@ public class EmployeeEvaluationTemplateItemTestData
     {
         Id = 1,
         Template = new EmployeeEvaluationTemplateDto { Id = 101, Description = "Template 1" },
+        Section = "Example Test Section",
+        Question = "Question 1"
+    };
+
+    public static EmployeeEvaluationTemplateItemDto employeeEvaluationTemplateItemDto2 = new EmployeeEvaluationTemplateItemDto
+    {
+        Id = 2,
+        Template = new EmployeeEvaluationTemplateDto { Id = 102, Description = "Template 2" },
+        Section = "Example Test Section",
+        Question = "Question 2"
+    };
+}
