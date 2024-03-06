302b923a4456b7479be4489de3031d90a81099c9
Reino Powell <rpowell@retrorabbit.co.za>
2024-03-06 15:55:01 +0200

    Tried new solution

--- RR.App.Tests/Controllers/HRIS/ClientControllerIntegrationTests.cs
+++ RR.App.Tests/Controllers/HRIS/ClientControllerIntegrationTests.cs
@@ -14,6 +14,10 @@ using RR.UnitOfWork.Entities.HRIS;
 using HRIS.Services.Interfaces;
 using HRIS.Services.Services;
 using IClientService = HRIS.Services.Interfaces.IClientService;
+using Microsoft.Extensions.Configuration;
+using RR.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 namespace RR.App.Tests.Controllers
 {
@@ -21,17 +25,34 @@ namespace RR.App.Tests.Controllers
     {
         private readonly WebApplicationFactory<RR.App.Program> _factory;
         private readonly HttpClient _client;
-        private readonly Mock<IClientService> _mockClientService;
 
         public ClientControllerIntegrationTests(WebApplicationFactory<RR.App.Program> factory)
         {
             _factory = factory;
-            _mockClientService = new Mock<IClientService>();
+
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets<Program>()
+                .AddEnvironmentVariables();
+
+            var configuration = configBuilder.Build();
+            var connectionString = configuration.GetConnectionString("Default");
+
             _client = _factory.WithWebHostBuilder(builder =>
             {
+                builder.ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("secrets.json", optional: false, reloadOnChange: true);
+                });
                 builder.ConfigureTestServices(services =>
                 {
-                    services.AddScoped<IClientService>(_ => _mockClientService.Object);
+                    services.AddDbContext<DatabaseContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("TestDatabase");
+                    });
+
+                    services.AddScoped<IUnitOfWork, RR.UnitOfWork.UnitOfWork>();
+                    services.AddScoped<IClientService, ClientService>();
                 });
             }).CreateClient();
         }
@@ -39,14 +60,6 @@ namespace RR.App.Tests.Controllers
         [Fact]
         public async Task GetAllClients_ReturnsOkResult()
         {
-            var clients = new List<ClientDto>
-            { 
-                new ClientDto { Id = 1, Name = "Client1"},
-                new ClientDto { Id = 2, Name = "Client2"}
-            };
-
-            _mockClientService.Setup(service => service.GetAllClients()).ReturnsAsync(clients);
-
             var response = await _client.GetAsync("/clients");
 
             response.EnsureSuccessStatusCode();
@@ -56,8 +69,6 @@ namespace RR.App.Tests.Controllers
         [Fact]
         public async Task GetAllClients_ReturnsNotFoundResultOnException()
         {
-            _mockClientService.Setup(service => service.GetAllClients()).ThrowsAsync(new Exception("An error occurred"));
-
             var response = await _client.GetAsync("/clients");
 
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
