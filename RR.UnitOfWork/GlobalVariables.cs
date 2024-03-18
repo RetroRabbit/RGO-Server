6db542ea382e259f401df5358fa69fe3850dd463
AWallsRR <awalls@retrorabbit.co.za>
2024-03-18 16:48:21 +0200

    Names space changes

--- RR.UnitOfWork/GlobalVariables.cs
+++ RR.UnitOfWork/GlobalVariables.cs
@@ -1,24 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore.Query.Internal;
-using Org.BouncyCastle.Bcpg;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace RR.UnitOfWork;
 
-namespace RR.UnitOfWork
+public static class GlobalVariables
 {
-    public static class GlobalVariables
-    {
-        private static int UserId;
+    private static int UserId;
 
-        public static void SetUserId(int id)
-        { 
-            UserId = id;
-        }
-        public static int GetUserId()
-        {
-            return UserId;
-        }
+    public static void SetUserId(int id)
+    { 
+        UserId = id;
+    }
+    public static int GetUserId()
+    {
+        return UserId;
     }
 }
