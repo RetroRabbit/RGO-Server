b26698a9038e210e006489ed55d693a3cd6673b3
Juanro-Minne <jminne@retrorabbit.co.za>
2024-05-03 11:44:12 +0200

    added usings

--- HRIS.Services/Services/WorkExperienceService.cs
+++ HRIS.Services/Services/WorkExperienceService.cs
@@ -1,14 +1,26 @@
 ﻿using HRIS.Models;
 using HRIS.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using HRIS.Models;
+using HRIS.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using RR.UnitOfWork;
 using RR.UnitOfWork.Entities.HRIS;
+
 
 namespace HRIS.Services.Services;
 
 public class WorkExperienceService : IWorkExperienceService
 {
-  
+    private readonly IUnitOfWork _db;
+    private readonly IErrorLoggingService _errorLoggingService;
+
+    public WorkExperienceService(IUnitOfWork db, IErrorLoggingService errorLoggingService)
+    {
+        _db = db;
+        _errorLoggingService = errorLoggingService;
+    }
+
     public Task<WorkExperienceDto> Get(WorkExperienceDto workExperience)
     {
         throw new NotImplementedException();
