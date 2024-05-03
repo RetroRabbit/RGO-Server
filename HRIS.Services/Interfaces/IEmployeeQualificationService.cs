8a09f2b6ab4fedd6e4add8529d194950043a4546
Dylan Vanhout <123621238+dvanhout89@users.noreply.github.com>
2024-05-03 10:02:45 +0200

    Added Employee Qualification Service Interface
    Added Employee Qualification Service Interface

---
+++ HRIS.Services/Interfaces/IEmployeeQualificationService.cs
@@ -0,0 +1,41 @@
+﻿using HRIS.Models;
+
+namespace HRIS.Services.Interfaces;
+
+public interface IEmployeeQualificationService
+{
+    /// <summary>
+    /// Save Employee Qualification
+    /// </summary>
+    /// <param name="employeeQualificationDto"></param>
+    /// <returns></returns>
+    Task<EmployeeQualificationDto> SaveEmployeeQualification(EmployeeQualificationDto employeeQualificationDto);
+
+    /// <summary>
+    /// Get Employee Qualification
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    Task<EmployeeQualificationDto> GetEmployeeQualification(string name);
+
+    /// <summary>
+    /// Get All Employee Qualifications
+    /// </summary>
+    /// <returns></returns>
+    Task<EmployeeQualificationDto> GetAllEmployeeQualifications();
+
+    /// <summary>
+    /// Update Employee Qualification
+    /// </summary>
+    /// <param name="employeeQualificationDto"></param>
+    /// <returns></returns>
+    Task<EmployeeQualificationDto> UpdateEmployeeQualification(EmployeeQualificationDto employeeQualificationDto);
+
+    /// <summary>
+    /// Delete Employee Qualification
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    Task<EmployeeQualificationDto> DeleteEmployeeQualification(string name);
+}
+
