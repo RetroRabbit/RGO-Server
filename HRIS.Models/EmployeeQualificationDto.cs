11fc6fc428443d2b6f9430b842af0291ed90a6e0
Dylan Vanhout <123621238+dvanhout89@users.noreply.github.com>
2024-05-03 12:35:31 +0200

    Update EmployeeQualificationDto.cs
    Updated EmployeeQualificationDto

--- HRIS.Models/EmployeeQualificationDto.cs
+++ HRIS.Models/EmployeeQualificationDto.cs
@@ -4,11 +4,11 @@ public class EmployeeQualificationDto
 {
     public int Id { get; set; }
     public int EmployeeId { get; set; }
-    public required string Qualification { get; set; }
-    public required string School { get; set; }
-    public required string Degree { get; set; }
-    public required string FieldOfStudy { get; set; }
-    public required string NQF { get; set; }
+    public required string? Qualification { get; set; }
+    public required string? School { get; set; }
+    public required string? Degree { get; set; }
+    public required string? FieldOfStudy { get; set; }
+    public required string? NQF { get; set; }
     public required DateTime StartDate { get; set; }
     public required DateTime? EndDate { get; set; }
 }
