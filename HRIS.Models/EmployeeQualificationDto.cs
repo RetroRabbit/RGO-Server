712c97496d25c718aa94a3ecbee6452d0a48d9b3
Dylan Vanhout <123621238+dvanhout89@users.noreply.github.com>
2024-05-03 10:01:17 +0200

    Added Employee Qualification Dto
    Added Employee Qualification Dto

---
+++ HRIS.Models/EmployeeQualificationDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HRIS.Models;
+
+public class EmployeeQualificationDto
+{
+    public int Id { get; set; }
+    public int EmployeeId { get; set; }
+    public required string HighestQualification { get; set; }
+    public required string School { get; set; }
+    public required string Degree { get; set; }
+    public required string FieldOfStudy { get; set; }
+    public required string NQF { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime? EndDate { get; set; }
+}
