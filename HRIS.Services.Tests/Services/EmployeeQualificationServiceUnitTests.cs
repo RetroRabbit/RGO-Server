b2385e57ed315de699a9708f9e7dbb75123b2dfb
Dylan Vanhout <123621238+dvanhout89@users.noreply.github.com>
2024-05-03 16:35:29 +0200

    Work in Progress
    Work in Progress
    Implementing Controller and Service Unit Testings

---
+++ HRIS.Services.Tests/Services/EmployeeQualificationServiceUnitTests.cs
@@ -0,0 +1,71 @@
+﻿using HRIS.Models;
+using HRIS.Services.Interfaces;
+using HRIS.Services.Services;
+using Moq;
+using RR.UnitOfWork;
+using RR.UnitOfWork.Entities.HRIS;
+using Xunit;
+
+namespace HRIS.Services.Tests.Services
+{
+    public class EmployeeQualificationServiceUnitTest
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly IEmployeeQualificationService _employeeQualificationService;
+
+        public EmployeeQualificationServiceUnitTest()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _employeeQualificationService = new EmployeeQualificationService(_unitOfWorkMock.Object);
+        }
+
+        [Fact]
+        public async Task SaveEmployeeQualificationValidInputReturnsNewQualification()
+        {
+            var qualificationDto = new EmployeeQualificationDto
+            {
+                Id = 1,
+                EmployeeId = 1,
+                Qualification = "Bachelor's Degree",
+                School = "University of Pretoria",
+                Degree = "Computer Science",
+                FieldOfStudy = "Software Engineering",
+                NQF = "NQF Level 7",
+                StartDate = new DateTime(2016, 9, 1),
+                EndDate = new DateTime(2020, 6, 30)
+            };
+        }
+
+        [Fact]
+        public async Task GetAllEmployeeQualificationsReturnsListOfQualifications()
+        {
+            var qualifications = new List<EmployeeQualificationDto> 
+            {
+                new EmployeeQualificationDto
+                {
+                    Id = 1,
+                    EmployeeId = 1,
+                    Qualification = "Bachelor's Degree",
+                    School = "University of Pretoria",
+                    Degree = "Computer Science",
+                    FieldOfStudy = "Software Engineering",
+                    NQF = "NQF Level 7",
+                    StartDate = new DateTime(2016, 9, 1),
+                    EndDate = new DateTime(2020, 6, 30)
+                },
+                new EmployeeQualificationDto
+                {
+                    Id = 2,
+                    EmployeeId = 1,
+                    Qualification = "Master's Degree",
+                    School = "University of Johannesburg",
+                    Degree = "Information Technology",
+                    FieldOfStudy = "Data Science",
+                    NQF = "NQF Level 9",
+                    StartDate = new DateTime(2018, 9, 1),
+                    EndDate = new DateTime(2022, 6, 30)
+                },
+            };
+        }
+    }
+}
