89ee1c68bb9eb4ce69baaeafbae1f5e6014530b4
Dylan Vanhout <123621238+dvanhout89@users.noreply.github.com>
2024-05-04 15:53:05 +0200

    Update EmployeeQualificationTestData.cs

--- RR.Tests.Data/Models/HRIS/EmployeeQualificationTestData.cs
+++ RR.Tests.Data/Models/HRIS/EmployeeQualificationTestData.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HRIS.Models;
+using RR.Tests.Data.Models.HRIS;
 
-namespace RR.Tests.Data.Models.HRIS
+namespace RGO.Tests.Data.Models;
+
+public static class EmployeeQualificationTestData
 {
-    internal class EmployeeQualificationTestData
+    public static EmployeeQualificationDto BachelorQualification => new EmployeeQualificationDto
     {
-    }
+        Id = 1,
+        EmployeeId = 1,
+        Qualification = "Bachelor's Degree",
+        School = "University of Pretoria",
+        Degree = "Computer Science",
+        FieldOfStudy = "Software Engineering",
+        NQF = "NQF Level 7",
+        StartDate = new DateTime(2016, 9, 1),
+        EndDate = new DateTime(2020, 6, 30)
+    };
+
+    public static EmployeeQualificationDto MasterQualification => new EmployeeQualificationDto
+    {
+        Id = 2,
+        EmployeeId = 1,
+        Qualification = "Master's Degree",
+        School = "University of Johannesburg",
+        Degree = "Information Technology",
+        FieldOfStudy = "Data Science",
+        NQF = "NQF Level 9",
+        StartDate = new DateTime(2021, 1, 1),
+        EndDate = new DateTime(2023, 12, 31)
+    };
+
+    public static List<EmployeeQualificationDto> AllQualifications => new List<EmployeeQualificationDto>
+    {
+        BachelorQualification,
+        MasterQualification
+    };
 }
+
