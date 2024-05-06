cbda8b805d46c841c8cc65552e0a9a4a6420cedd
Lucien van Wyk <lvanwyk@retrorabbit.co.za>
2024-05-06 15:31:05 +0200

    Commit Model, Enum and Interface

---
+++ HRIS.Models/EmployeeSalaryDetailsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRIS.Models.Enums;
+
+namespace HRIS.Models;
+
+public class EmployeeSalaryDetailsDto
+{
+    public int Id { get; set; }
+    public EmployeeDto? Employee { get; set; }
+    public float? Salary { get; set; }
+    public float? MinSalary { get; set; }
+    public float? MaxSalary { get; set; }
+    public float? Remuneration { get; set; }
+    public EmployeeSalaryBand? Band { get; set; }
+    public string? Contribution { get; set; }
+}
---
+++ HRIS.Models/Enums/EmployeeSalaryBand.cs
@@ -0,0 +1,10 @@
+﻿namespace HRIS.Models.Enums;
+
+public enum EmployeeSalaryBand
+{
+    Level1,
+    Level2,
+    Level3,
+    Level4,
+    Level5
+}
