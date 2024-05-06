0de184adb53f2e8eea0f3e378e815ba91d72ad1a
Juanro-Minne <jminne@retrorabbit.co.za>
2024-05-06 14:26:38 +0200

    added references and unit test file with migration

---
+++ HRIS.Services.Tests/Services/WorkExperienceServiceUnitTest.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq.Expressions;
+using HRIS.Models;
+using HRIS.Models.Enums;
+using HRIS.Services.Interfaces;
+using HRIS.Services.Services;
+using MockQueryable.Moq;
+using Moq;
+using RR.UnitOfWork;
+using RR.UnitOfWork.Entities.HRIS;
+using Xunit;
+
+namespace RGO.Tests.Services;
+
+public class WorkExperienceServiceUnitTest
+{
+    private readonly WorkExperienceService _workExperienceService;
+    private readonly WorkExperienceDto _workExperienceDto;
+    private readonly Mock<IUnitOfWork> _mockDb;
+    private readonly Mock<IErrorLoggingService> _errorLogggingServiceMock;
+
+    public WorkExperienceServiceUnitTest()
+    {
+        _mockDb = new Mock<IUnitOfWork>();
+        _errorLogggingServiceMock = new Mock<IErrorLoggingService>();
+
+
+        //_workExperienceDto = new WorkExperienceDto
+        //{
+        //    Id = 1,
+        //    Title = "Senior Developer",
+        //    EmploymentType = "Permanent",
+        //    CompanyName = "Retro Rabbit",
+        //    Location = "Pretoria",
+        //    EmployeeId = 1,
+        //    StartDate = new DateOnly(2022, 1, 1),
+        //    EndDate = new DateOnly(2024, 1, 1),
+        //};
+    }
+
+    //[Fact]
+    //public async Task CheckIfExistsFailTest()
+    //{
+    //    _mockDb.Setup(x => x.WorkExperience.Any(It.IsAny<Expression<Func<WorkExperience, bool>>>()))
+    //           .ReturnsAsync(false);
+
+    //    var exists = await _workExperienceService.CheckIfExists(new WorkExperienceDto
+    //    {
+    //        Id = 1,
+    //        Title = "Senior Developer",
+    //        EmploymentType = "Permanent",
+    //        CompanyName = "Retro Rabbit",
+    //        Location = "Pretoria",
+    //        EmployeeId = 1,
+    //        StartDate = new DateOnly(2022, 1, 1),
+    //        EndDate = new DateOnly(2024, 1, 1),
+    //    });
+
+    //    Assert.False(exists);
+    //}
+
+    //[Fact]
+    //public async Task CheckIfExistsPassTest()
+    //{
+    //    _mockDb.Setup(x => x.WorkExperience.Any(It.IsAny<Expression<Func<WorkExperience, bool>>>()))
+    //           .ReturnsAsync(false);
+
+    //    var exists = await _workExperienceService.CheckIfExists(new WorkExperienceDto
+    //    {
+    //        Id = 1,
+    //        Title = "Senior Developer",
+    //        EmploymentType = "Permanent",
+    //        CompanyName = "Retro Rabbit",
+    //        Location = "Pretoria",
+    //        EmployeeId = 1,
+    //        StartDate = new DateOnly(2022, 1, 1),
+    //        EndDate = new DateOnly(2024, 1, 1),
+    //    });
+
+    //    Assert.True(exists);
+    //}
+}
