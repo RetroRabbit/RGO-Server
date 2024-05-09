03167f1c56b857d832f30de590e812fca8fc00be
Lucien <lvanwyk@retrorabbit.co.za>
2024-05-09 16:17:36 +0200

    Completed Service and Creating Controller

---
+++ RR.App/Controllers/HRIS/EmployeeSalaryDetailsController.cs
@@ -0,0 +1,68 @@
+﻿using HRIS.Models;
+using HRIS.Models.Enums;
+using HRIS.Services.Interfaces;
+using HRIS.Services.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace RR.App.Controllers.HRIS;
+
+[Route("employee-salary")]
+[ApiController]
+public class EmployeeSalaryDetailsController : ControllerBase
+{
+    private readonly IEmployeeSalarayDetailsService _employeeSalarayDetailsService;
+    public EmployeeSalaryDetailsController(IEmployeeSalarayDetailsService employeeSalarayDetailsService) 
+    {
+        _employeeSalarayDetailsService = employeeSalarayDetailsService;
+    }
+
+    [Authorize(Policy = "AllRolesPolicy")]
+    [HttpGet("")]
+    public async Task<IActionResult> GetAllEmployeeSalaries()
+    {
+        try
+        {
+            var employeeSalaries = await _employeeSalarayDetailsService.GetAllEmployeeSalaries();
+            return Ok(employeeSalaries);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while fetching the employee documents.");
+        }
+    }
+
+    [Authorize(Policy = "AllRolesPolicy")]
+    [HttpGet("{employeeId}")]
+    public async Task<IActionResult> GetEmployeeSalary(int employeeId)
+    {
+        try
+        {
+            var employeeSalaries = await _employeeSalarayDetailsService.GetEmployeeSalary(employeeId);
+            return Ok(employeeSalaries);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "An error occurred while fetching employee documents.");
+        }
+    }
+
+    [Authorize(Policy = "AllRolesPolicy")]
+    [HttpPost()]
+    public async Task<IActionResult> AddEmployeeSalary([FromBody] EmployeeSalaryDetailsDto employeeSalaryDetailsDto)
+    {
+        try
+        {
+            var employeeSalaries = await _employeeSalarayDetailsService.SaveEmployeeSalary(employeeSalaryDetailsDto);
+            return CreatedAtAction(nameof(AddEmployeeSalary), new { employeeId = employeeSalaries.EmployeeId }, employeeSalaries);
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message.Contains("exists"))
+                return Problem("Unexceptable", "Unexceptable", 406, "User Salary Exists");
+
+            return NotFound(ex.Message);
+        }
+    }
+}
