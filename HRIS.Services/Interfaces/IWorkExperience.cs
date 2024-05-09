7eaa0ef93d6159dece4ff994ddb7ba12151fce76
Juanro-Minne <jminne@retrorabbit.co.za>
2024-05-09 09:31:58 +0200

    summary

--- HRIS.Services/Interfaces/IWorkExperience.cs
+++ HRIS.Services/Interfaces/IWorkExperience.cs
@@ -4,8 +4,31 @@ namespace HRIS.Services.Interfaces;
 
 public interface IWorkExperienceService
 {
-    Task<WorkExperienceDto> Save(WorkExperienceDto workExperience);
+    /// <summary>
+    /// Save Employee work experience
+    /// </summary>
+    /// <param name="workExperienceDto"></param>
+    /// <returns>Employee work experience</returns>
+    Task<WorkExperienceDto> Save(WorkExperienceDto workExperienceDto);
+
+    /// <summary>
+    /// Delete Employee work experience
+    /// </summary>
+    /// <param name="workExperienceDto"></param>
+    /// <returns>Delete Employee work experience</returns>
     Task<WorkExperienceDto> Delete(int workExperienceId);
+    /// <summary>
+    /// Update Employee work experience
+    /// </summary>
+    /// <param name="workExperienceDto"></param>
+    /// <returns>Update Employee work experience</returns>
     Task<WorkExperienceDto> Update(WorkExperienceDto workExperience);
+
+    /// <summary>
+    /// Get  specific work experience data via the ID
+    /// </summary>
+    /// <param name="employeeId"></param>
+    /// <param name="status"></param>
+    /// <returns>Employee Work experience</returns>
     Task<WorkExperienceDto> GetWorkExperienceById(int id);
 }
