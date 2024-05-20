50cd78cac2a2ba647efeddb56c55f04c89bd3596
Dylan Vanhout <123621238+dvanhout89@users.noreply.github.com>
2024-05-20 09:43:52 +0200

    Added Middleware for Security Headers
    Added Middleware for Security Headers

---
+++ HRIS.Services/Middleware/SecurityHeadersMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace HRIS.Services.Middleware;
+
+public class SecurityHeadersMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public SecurityHeadersMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Add security headers to the response
+        context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+        context.Response.Headers.Add("X-Frame-Options", "DENY");
+        context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+        context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
+        context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+
+        await _next(context);
+    }
+}
---
+++ HRIS.Services/Middleware/SecurityHeadersMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace HRIS.Services.Middleware;
+public static class SecurityHeadersMiddlewareExtensions
+{
+    public static IApplicationBuilder UseSecurityHeaders(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<SecurityHeadersMiddleware>();
+    }
+}
