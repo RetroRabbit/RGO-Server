86aadae98c958997f2d4fcde0f1ef212566eb79a
Reino Powell <rpowell@retrorabbit.co.za>
2024-05-22 09:12:46 +0200

    refactor

--- HRIS.Models/WorkExperienceDto.cs
+++ HRIS.Models/WorkExperienceDto.cs
@@ -2,7 +2,7 @@
 public class WorkExperienceDto
 {
     public int Id { get; set; }
-    public string ClientName { get; set; }
+    public string? ClientName { get; set; }
     public string? ProjectName { get; set; }
     public List<string>? SkillSet { get; set; }
     public List<string>? Software { get; set; }
