364e02a57a665426871127323492877b0e8fd6e6
Wimpie Scholtz <wscholtz@retrorabbit.co.za>
2024-06-18 16:20:30 +0200

    Fixed AuthController

--- RR.App/Controllers/HRIS/AuthenticationController.cs
+++ RR.App/Controllers/HRIS/AuthenticationController.cs
@@ -1,10 +1,6 @@
-﻿using HRIS.Models;
-using HRIS.Services.Interfaces;
-using HRIS.Services.Services;
+﻿using HRIS.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using RR.UnitOfWork.Entities.HRIS;
-using System.Security.Claims;
 
 namespace RR.App.Controllers.HRIS;
 
@@ -34,63 +30,4 @@ public class AuthenticationController : ControllerBase
         }
     }
 
-    [AllowAnonymous]
-    [HttpPost("register")]
-    public async Task<IActionResult> RegisterEmployee([FromBody] EmployeeDto newEmployee)
-    {
-        try
-        {
-            var userExists = await _authService.CheckUserExist(newEmployee.Email!);
-
-            if (userExists) throw new Exception("Employee already exists");
-
-            var token = await _authService.RegisterEmployee(newEmployee);
-
-            return CreatedAtAction(nameof(RegisterEmployee), newEmployee);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
-    }
-
-    [Authorize(Policy = "AllRolesPolicy")]
-    [HttpGet("roles")]
-    public async Task<IActionResult> GetUserRoles([FromQuery] string? email)
-    {
-        try
-        {
-            var claimsIdentity = User.Identity as ClaimsIdentity;
-            var accessTokenEmail = claimsIdentity?.FindFirst(ClaimTypes.Email)?.Value;
-            var role = claimsIdentity?.FindFirst(ClaimTypes.Role)?.Value;
-
-            if (!string.IsNullOrEmpty(accessTokenEmail))
-            {
-                var emailToUse = string.IsNullOrEmpty(email)
-                ? claimsIdentity?.FindFirst(ClaimTypes.Email)?.Value
-                : email;
-
-                if ("SuperAdmin" == role || "Admin" == role || "Talent" == role || "Journey" == role)
-                {
-                    var roles = await _authService.GetUserRoles(emailToUse ?? "");
-
-                    return Ok(roles);
-                }
-
-
-                if (emailToUse == accessTokenEmail)
-                {
-                    var roles = await _authService.GetUserRoles(emailToUse ?? "");
-
-                    return Ok(roles);
-                }
-            }
-
-            return NotFound("Tampering found!");
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
-    }
 }
