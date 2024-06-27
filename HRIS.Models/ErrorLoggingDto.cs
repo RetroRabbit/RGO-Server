ffea9eff0f03f641a2a251a134433a67c222f2c2
Juanro-Minne <jminne@retrorabbit.co.za>
2024-06-27 11:19:30 +0200

    added new code

--- HRIS.Models/ErrorLoggingDto.cs
+++ HRIS.Models/ErrorLoggingDto.cs
@@ -8,9 +8,12 @@ namespace ATS.Models
         public DateTime DateOfIncident { get; set; }
         public string StackTrace { get; set; }
         public string Message { get; set; }
-
-        // Additional properties for error details
+        public string IpAddress { get; set; }
         public int? StatusCode { get; set; }
         public string ErrorDetails { get; set; }
+        public string RequestUrl { get; set; }
+        public string RequestMethod { get; set; }
+        public string? RequestContentType { get; set; }
+        public object RequestBody { get; set; }
     }
 }
--- RR.App/Middleware/ExceptionHandlingMiddleware.cs
+++ RR.App/Middleware/ExceptionHandlingMiddleware.cs
@@ -6,6 +6,12 @@ using RR.UnitOfWork.Entities;
 using ATS.Models;
 using HRIS.Services;
 using HRIS.Services.Services;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Hris.Middleware
 {
@@ -13,6 +19,7 @@ namespace Hris.Middleware
     {
         private readonly RequestDelegate _next;
         private readonly IErrorLoggingService _errorLoggingService;
+        private readonly MemoryStream _recyclableMemoryStreamManager;
 
         public ExceptionHandlingMiddleware(RequestDelegate next, IErrorLoggingService errorLoggingService)
         {
@@ -32,7 +39,7 @@ namespace Hris.Middleware
             }
         }
 
-        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private async Task<Task> HandleExceptionAsync(HttpContext context, Exception exception)
         {
             var statusCode = StatusCodes.Status500InternalServerError;
             var message = "Internal Server Error. Please try again later.";
@@ -51,15 +58,41 @@ namespace Hris.Middleware
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = statusCode;
 
+            var type = exception.GetType().Name;
+            var url = context.Request.GetDisplayUrl();
+            var error = await GetRequestInfo(context);
+
             var errorResponse = new ErrorLoggingDto
             {
                 StatusCode = statusCode,
-                Message = message
+                Message = message,
+                
             };
 
             var errorJson = System.Text.Json.JsonSerializer.Serialize(errorResponse);
+
             return context.Response.WriteAsync(errorJson);
         }
 
+        private async Task<ErrorLoggingDto> GetRequestInfo(HttpContext context)
+        {
+            var log = new ErrorLoggingDto();
+            var bodyAsString = string.Empty;
+            context.Request.EnableBuffering();
+
+            if (context.Request.Body != null)
+            {
+                using StreamReader requestStream = new StreamReader(context.Request.Body);
+                bodyAsString = await requestStream.ReadToEndAsync();
+            }
+
+            log.IpAddress = $"{context.Request.Host.Host}";
+            log.RequestUrl = $"{context.Request.GetDisplayUrl()}";
+            log.RequestMethod = $"{context.Request.Method}";
+            log.RequestContentType = context.Request.ContentType;
+            log.RequestBody = bodyAsString;
+
+            return log;
+        }
     }
 }
