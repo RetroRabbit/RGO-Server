0250a7c0dd21129db34ea58a0e972384e80cc3ba
Reino Powell <rpowell@retrorabbit.co.za>
2024-07-02 11:00:29 +0200

    Removed all try catches

--- RR.App/Controllers/HRIS/ChartsController.cs
+++ RR.App/Controllers/HRIS/ChartsController.cs
@@ -21,8 +21,6 @@ public partial class ChartsController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAllCharts()
     {
-        try
-        {
             var chart = await _chartService.GetAllCharts();
 
             for(int i = 0; i < chart.Count; i++)
@@ -32,21 +30,13 @@ public partial class ChartsController : ControllerBase
                     chart[i].DataTypes![j] = CapitalLetters().Replace(chart[i].DataTypes![j], "$1 $2");
                 }
             }
-
             return Ok(chart);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
     }
 
     [Authorize(Policy = "AdminOrTalentOrJourneyOrSuperAdminPolicy")]
     [HttpGet("employee")]
     public async Task<IActionResult> GetEmployeeCharts([FromQuery] int employeeId)
     {
-        try
-        {
             var charts = await _chartService.GetEmployeeCharts(employeeId);
 
             for (int i = 0; i < charts.Count; i++)
@@ -56,85 +46,46 @@ public partial class ChartsController : ControllerBase
                     charts[i].DataTypes![j] = CapitalLetters().Replace(charts[i].DataTypes![j], "$1 $2");
                 }
             }
-
             return Ok(charts);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
     }
 
     [Authorize(Policy = "AdminOrTalentOrJourneyOrSuperAdminPolicy")]
     [HttpPost]
-    public async Task<IActionResult> CreateChart([FromQuery] List<string> dataType, [FromQuery] List<string> roles,
-                                                 string chartName, string chartType, int employeeId)
+    public async Task<IActionResult> CreateChart([FromQuery] List<string> dataType, [FromQuery] List<string> roles, string chartName, string chartType, int employeeId)
     {
-        try
-        {
             var response = await _chartService.CreateChart(dataType, roles, chartName, chartType, employeeId);
-
             return Ok(response);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
     }
 
     [Authorize(Policy = "AdminOrTalentOrJourneyOrSuperAdminPolicy")]
     [HttpGet("data")]
     public async Task<IActionResult> GetChartData([FromQuery] List<string> dataTypes)
     {
-        try
-        {
             var chartData = await _chartService.GetChartData(dataTypes);
             return Ok(chartData);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
     }
 
     [Authorize(Policy = "AdminOrTalentOrJourneyOrSuperAdminPolicy")]
     [HttpPut]
     public async Task<IActionResult> UpdateChartData(ChartDto chartDto)
     {
-        try
-        {
             var saveChart = await _chartService.UpdateChart(chartDto);
             if (saveChart == null) throw new Exception("Chart data not updated");
-
             return Ok(saveChart);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
     }
 
     [Authorize(Policy = "AdminOrTalentOrJourneyOrSuperAdminPolicy")]
     [HttpDelete]
     public async Task<IActionResult> DeleteChart([FromQuery] int chartId)
     {
-        try
-        {
             var deletedChart = await _chartService.DeleteChart(chartId);
             return Ok(deletedChart);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
     }
 
     [Authorize(Policy = "AdminOrTalentOrJourneyOrSuperAdminPolicy")]
     [HttpGet("column")]
     public IActionResult GetColumns()
     {
-        try
-        {
             var columns = _chartService.GetColumnsFromTable();
 
             for(int i = 0; i < columns.Length;  i++)
@@ -143,19 +94,12 @@ public partial class ChartsController : ControllerBase
             }
 
             return Ok(columns);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
     }
 
     [Authorize(Policy = "AdminOrTalentOrJourneyOrSuperAdminPolicy")]
     [HttpGet("report/export")]
     public async Task<IActionResult> ExportCsv([FromQuery] List<string> dataTypes)
     {
-        try
-        {
             var csvData = await _chartService.ExportCsvAsync(dataTypes);
 
             if (csvData == null || csvData.Length == 0)
@@ -164,11 +108,6 @@ public partial class ChartsController : ControllerBase
             var fileName = "Report.csv";
 
             return File(csvData, "text/csv", fileName);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
     }
 
     [GeneratedRegex("([a-z])([A-Z])")]
