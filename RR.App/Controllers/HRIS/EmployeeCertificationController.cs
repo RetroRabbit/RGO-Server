dde4d384d48a0fae9962e03f09ce66b9a374c8fc
Wimpie Scholtz <wscholtz@retrorabbit.co.za>
2024-07-03 15:54:09 +0200

    Removed try catch from Employee Cert controller

--- RR.App/Controllers/HRIS/EmployeeCertificationController.cs
+++ RR.App/Controllers/HRIS/EmployeeCertificationController.cs
@@ -23,98 +23,61 @@ public class EmployeeCertificationController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAllEmployeelCertiificates(int employeeId)
     {
-        try
+        if (_identity.Role is "SuperAdmin" or "Admin" or "Talent" or "Journey")
         {
-            if (_identity.Role is "SuperAdmin" or "Admin" or "Talent" or "Journey")
-            {
-                var certificates = await _employeeCertificationService.GetAllEmployeeCertifications(employeeId);
-                return Ok(certificates);
-            }
+            var certificates = await _employeeCertificationService.GetAllEmployeeCertifications(employeeId);
+            return Ok(certificates);
+        }
             
-            if (employeeId == _identity.EmployeeId)
-            {
-                var certificates = await _employeeCertificationService.GetAllEmployeeCertifications(employeeId);
-                return Ok(certificates);
-            }
-            return NotFound("User data being accessed does not match user making the request.");
-        }
-        catch(Exception ex)
+        if (employeeId == _identity.EmployeeId)
         {
-            return BadRequest(ex.Message);
+            var certificates = await _employeeCertificationService.GetAllEmployeeCertifications(employeeId);
+            return Ok(certificates);
         }
+
+        return NotFound("User data being accessed does not match user making the request.");
     }
 
     [Authorize(Policy = "AdminOrSuperAdminPolicy")]
     [HttpPost]
     public async Task<IActionResult> SaveEmployeeCertificate(EmployeeCertificationDto employeeCertificationDto)
-    {
-        try
-        {
-            var certificate = await _employeeCertificationService.SaveEmployeeCertification(employeeCertificationDto);
-            return Ok(certificate);
-
-        }
-        catch(Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+    { 
+        var certificate = await _employeeCertificationService.SaveEmployeeCertification(employeeCertificationDto);
+        return Ok(certificate);
     }
 
     [Authorize(Policy = "AdminOrSuperAdminPolicy")]
     [HttpGet("employee-certificate")]
     public async Task<IActionResult> GetEmployeeCertificate(int employeeId, int certificationId)
     {
-        try
+        if (_identity.Role is "SuperAdmin" or "Admin" or "Talent" or "Journey")
         {
-            if (_identity.Role is "SuperAdmin" or "Admin" or "Talent" or "Journey")
-            {
-                var certificate = await _employeeCertificationService.GetEmployeeCertification(employeeId, certificationId);
-                return Ok(certificate);
-            }
+            var certificate = await _employeeCertificationService.GetEmployeeCertification(employeeId, certificationId);
+            return Ok(certificate);
+        }
             
-            if (employeeId == _identity.EmployeeId)
-            {
-                var certificate = await _employeeCertificationService.GetEmployeeCertification(employeeId, certificationId);
-                return Ok(certificate);
-            }
-
-            return NotFound("User data being accessed does not match user making the request.");
-        }
-        catch(Exception ex)
+        if (employeeId == _identity.EmployeeId)
         {
-            return BadRequest(ex.Message);
+            var certificate = await _employeeCertificationService.GetEmployeeCertification(employeeId, certificationId);
+            return Ok(certificate);
         }
+
+        return NotFound("User data being accessed does not match user making the request.");
     }
 
     [Authorize(Policy = "AdminOrSuperAdminPolicy")]
     [HttpDelete]
     public async Task<IActionResult> DeleteEmployeeCertificate(int id)
-    {
-        try
-        {
-            var certificate = await _employeeCertificationService.DeleteEmployeeCertification(id);
-            return Ok(certificate);
-
-        }
-        catch(Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+{
+        var certificate = await _employeeCertificationService.DeleteEmployeeCertification(id);
+        return Ok(certificate);
     }
 
     [Authorize(Policy = "AdminOrSuperAdminPolicy")]
     [HttpPut]
     public async Task<IActionResult> UpdateCertificate(EmployeeCertificationDto employeeCertificationDto)
     {
-        try
-        {
-            var certificate = await _employeeCertificationService.UpdateEmployeeCertification(employeeCertificationDto);
-            return Ok(certificate);
-
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        var certificate = await _employeeCertificationService.UpdateEmployeeCertification(employeeCertificationDto);
+        return Ok(certificate);
     }
 }
