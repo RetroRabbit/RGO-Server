78e546b77f928e12cf86add5559f8978eaf636df
Ntuthuko Zikalala <nzikalala@retroreabbit.co.za>
2024-07-05 14:11:01 +0200

    refactored environemt variables

---
+++ HRIS.Services/Helpers/EnvironmentVariableHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRIS.Services.Helpers
+{
+    public static class EnvironmentVariableHelper
+    {
+
+        public static string CONNECTION_STRINGS_DEFAULT => Environment.GetEnvironmentVariable("ConnectionStrings__Default");
+        public static string AUTH_MANAGEMENT_ISSUER => Environment.GetEnvironmentVariable("AuthManagement__Issuer");
+        public static string AUTH_MANAGEMENT_CLIENT_SECRET => Environment.GetEnvironmentVariable("AuthManagement__ClientSecret");
+        public static string AUTH_MANAGEMENT_CLIENT_ID => Environment.GetEnvironmentVariable("AuthManagement__ClientId");
+        public static string AUTH_MANAGEMENT_AUDIENCE => Environment.GetEnvironmentVariable("AuthManagement__Audience");
+        public static string SMTP_HOST => Environment.GetEnvironmentVariable("SMTP__Host");
+        public static string SMTP_NAME => Environment.GetEnvironmentVariable("SMTP__Name");
+        public static string SMTP_MAIL => Environment.GetEnvironmentVariable("SMTP__Mail");
+        public static string SMTP_PASSWORD => Environment.GetEnvironmentVariable("SMTP__Password");
+        public static string SMTP_PORT => Environment.GetEnvironmentVariable("SMTP__Port");
+    }
+}
