307ffa0ff3be46267553ddd3b9c084d5c247ab2f
Mathilda Shongwe <mshongwe@retrorabbit.co.za>
2024-07-06 10:45:58 +0200

    Tests completed

--- RR.App.Tests/Controllers/HRIS/EmployeeDataControllerUnitTests.cs
+++ RR.App.Tests/Controllers/HRIS/EmployeeDataControllerUnitTests.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RR.App.Controllers.HRIS;
+using RR.App.Tests.Helper;
 using RR.Tests.Data;
 using RR.Tests.Data.Models.HRIS;
 using RR.UnitOfWork.Entities.HRIS;
@@ -14,22 +15,19 @@ using System.Security.AccessControl;
 using Xunit;
 
 namespace RR.App.Tests.Controllers.HRIS;
-
 public class EmployeeDataControllerUnitTests
 {
     private readonly Mock<IEmployeeDataService> _employeeDataServiceMock;
     private readonly EmployeeDataController _controller;
-    private readonly EmployeeDataController _controllers;
-
     private readonly List<EmployeeDataDto> _employeeDataDtoList;
     private readonly EmployeeDataDto _employeeDataDto;
+    private readonly EmployeeDataDto _employeeDataDto1;
     private readonly Mock<AuthorizeIdentityMock> _identity;
     public EmployeeDataControllerUnitTests()
     {
         _employeeDataServiceMock = new Mock<IEmployeeDataService>();
-        _identity = new Mock<AuthorizeIdentityMock>();
-
-        _controller = new EmployeeDataController(new AuthorizeIdentityMock(2), _employeeDataServiceMock.Object);
+        _identity = new Mock<AuthorizeIdentityMock>(MockBehavior.Strict, null, null);
+        _controller = new EmployeeDataController(_identity.Object, _employeeDataServiceMock.Object);
 
         _employeeDataDtoList = new List<EmployeeDataDto>
         {
@@ -38,6 +36,7 @@ public class EmployeeDataControllerUnitTests
         };
         
         _employeeDataDto = EmployeeDataTestData.EmployeeDataOne.ToDto();
+        _employeeDataDto1 = EmployeeDataTestData.EmployeeDataThree.ToDto();
     }
 
     [Fact]
@@ -54,6 +53,7 @@ public class EmployeeDataControllerUnitTests
         var okResult = Assert.IsType<OkObjectResult>(result);
         var returnValue = Assert.IsType<List<EmployeeDataDto>>(okResult.Value);
         Assert.Equal(_employeeDataDtoList, returnValue);
+        _employeeDataServiceMock.Verify(service => service.GetAllEmployeeData(_employeeDataDto.EmployeeId), Times.Once);
     }
 
     [Fact]
@@ -61,8 +61,8 @@ public class EmployeeDataControllerUnitTests
     {
         _identity.Setup(i => i.Role).Returns("Developer");
         _identity.SetupGet(i => i.EmployeeId).Returns(5);
-        
-        var result = await _controllers.GetEmployeeData(_employeeDataDto.EmployeeId);
+
+        var result = await MiddlewareHelperUnitTests.SimulateHandlingExceptionMiddlewareAsync(async () => await _controller.GetEmployeeData(2));
 
         var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
         Assert.Equal(StatusCodes.Status404NotFound, notFoundResult.StatusCode);
@@ -70,7 +70,24 @@ public class EmployeeDataControllerUnitTests
     }
 
     [Fact]
-    public async Task SaveEmployeeDataReturnsOkResult()
+    public async Task SaveUnauthorizedAccess()
+    {
+        _identity.Setup(i => i.Role).Returns("Developer");
+        _identity.SetupGet(i => i.EmployeeId).Returns(5);
+
+        var newController = new EmployeeDataController(new AuthorizeIdentityMock(), _employeeDataServiceMock.Object);
+
+        _employeeDataServiceMock.Setup(service => service.SaveEmployeeData(_employeeDataDto1))
+                              .ThrowsAsync(new Exception("Unauthorized Access"));
+
+        var result = await MiddlewareHelperUnitTests.SimulateHandlingExceptionMiddlewareAsync(async () => await newController.SaveEmployeeData(_employeeDataDto1));
+
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+        Assert.Equal("Unauthorized Access", notFoundResult.Value);
+    }
+
+    [Fact]
+    public async Task SaveEmployeeDataReturnsOkResults()
     {
         _identity.Setup(i => i.Role).Returns("SuperAdmin");
         _identity.SetupGet(i => i.EmployeeId).Returns(2);
@@ -83,6 +100,7 @@ public class EmployeeDataControllerUnitTests
         var okResult = Assert.IsType<OkObjectResult>(result);
         var returnValue = Assert.IsType<EmployeeDataDto>(okResult.Value);
         Assert.Equal(_employeeDataDto, returnValue);
+        _employeeDataServiceMock.Verify(service => service.SaveEmployeeData(_employeeDataDto), Times.Once);
     }
 
     [Fact]
@@ -99,6 +117,22 @@ public class EmployeeDataControllerUnitTests
         var okResult = Assert.IsType<OkObjectResult>(result);
         var returnValue = Assert.IsType<EmployeeDataDto>(okResult.Value);
         Assert.Equal(_employeeDataDto, returnValue);
+        _employeeDataServiceMock.Verify(service => service.UpdateEmployeeData(_employeeDataDto), Times.Once);
+    }
+
+    [Fact]
+    public async Task updateUnauthorized()
+    {
+        var newController = new EmployeeDataController(new AuthorizeIdentityMock(), _employeeDataServiceMock.Object);
+
+        _employeeDataServiceMock.Setup(service => service.UpdateEmployeeData(_employeeDataDto))
+                              .ThrowsAsync(new Exception("Unauthorized Access"));
+
+        var result = await MiddlewareHelperUnitTests.SimulateHandlingExceptionMiddlewareAsync(async () => await newController.UpdateEmployeeData(_employeeDataDto));
+
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+        Assert.Equal(StatusCodes.Status404NotFound, notFoundResult.StatusCode);
+        Assert.Equal("Unauthorized Access", notFoundResult.Value);
     }
 
     [Fact]
@@ -112,5 +146,6 @@ public class EmployeeDataControllerUnitTests
         var okResult = Assert.IsType<OkObjectResult>(result);
         var returnValue = Assert.IsType<EmployeeDataDto>(okResult.Value);
         Assert.Equal(_employeeDataDto, returnValue);
+        _employeeDataServiceMock.Verify(service => service.DeleteEmployeeData(_employeeDataDto.Id), Times.Once);
     }
 }
--- RR.App/Controllers/HRIS/EmployeeDataController.cs
+++ RR.App/Controllers/HRIS/EmployeeDataController.cs
@@ -26,7 +26,7 @@ public class EmployeeDataController : ControllerBase
     public async Task<IActionResult> GetEmployeeData([FromQuery] int id)
     {
            if (_identity.Role is not ("SuperAdmin" or "Admin" or "Talent" or "Journey") && id != _identity.EmployeeId)
-           throw new CustomException("User data being accessed does not match user making the request.");
+           throw new CustomException("Unauthorized Access");
       
            var data = await _employeeDataService.GetAllEmployeeData(id);
            return Ok(data);
@@ -37,7 +37,7 @@ public class EmployeeDataController : ControllerBase
     public async Task<IActionResult> SaveEmployeeData([FromBody] EmployeeDataDto employeeDataDto)
     {
           if (_identity.Role is not ("SuperAdmin" or "Admin" or "Talent" or "Journey") && employeeDataDto.EmployeeId != _identity.EmployeeId)
-          throw new CustomException("User data being accessed does not match user making the request.");
+          throw new CustomException("Unauthorized Access");
 
           var data = await _employeeDataService.SaveEmployeeData(employeeDataDto);
           return Ok(data);
@@ -48,7 +48,7 @@ public class EmployeeDataController : ControllerBase
     public async Task<IActionResult> UpdateEmployeeData([FromBody] EmployeeDataDto employeeDataDto)
     {
          if(_identity.Role is not ("SuperAdmin" or "Admin" or "Talent" or "Journey") && employeeDataDto.EmployeeId != _identity.EmployeeId)
-         throw new CustomException("User data being accessed does not match user making the request.");
+         throw new CustomException("Unauthorized Access");
 
          var data = await _employeeDataService.UpdateEmployeeData(employeeDataDto);
          return Ok(data);
