f1b6f3e72b2e0233a6f686b0a803f59da56aee23
Juanro-Minne <jminne@retrorabbit.co.za>
2024-07-10 11:12:52 +0200

    removed open lines

--- HRIS.Models/ChurnRateDataCard.cs
+++ HRIS.Models/ChurnRateDataCard.cs
@@ -9,8 +9,6 @@ public class ChurnRateDataCardDto
     public double BusinessSupportChurnRate { get; set; }
     public string? Month { get; set; }
     public int Year { get; set; }
-
     public double ChurnRateDifference { get; set; }
-
     public bool IsIncrease { get; set; }
 }
