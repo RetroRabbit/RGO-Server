85e0d9c601e2cc90e839d669d4a195dba1aaa8c8
Juanro-Minne <jminne@retrorabbit.co.za>
2024-07-10 11:11:59 +0200

    removed open spaces

--- HRIS.Models/ChurnRateDataCard.cs
+++ HRIS.Models/ChurnRateDataCard.cs
@@ -3,17 +3,11 @@
 public class ChurnRateDataCardDto
 {
     public double ChurnRate { get; set; }
-
     public double DeveloperChurnRate { get; set; }
-
     public double DesignerChurnRate { get; set; }
-
     public double ScrumMasterChurnRate { get; set; }
-
     public double BusinessSupportChurnRate { get; set; }
-
     public string? Month { get; set; }
-
     public int Year { get; set; }
 
     public double ChurnRateDifference { get; set; }
