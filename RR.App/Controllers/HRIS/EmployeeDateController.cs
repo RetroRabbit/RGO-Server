5907df78497de4519644d4cd5bb7f1ee46349f71
Reino Powell <rpowell@retrorabbit.co.za>
2024-07-12 09:02:10 +0200

    Removed Comments

--- RR.App/Controllers/HRIS/EmployeeDateController.cs
+++ RR.App/Controllers/HRIS/EmployeeDateController.cs
@@ -10,30 +10,16 @@ namespace RR.App.Controllers.HRIS;
 public class EmployeeDateController : ControllerBase
 {
     private readonly IEmployeeDateService _employeeDateService;
-    //private readonly IEmployeeService _employeeService;
 
     public EmployeeDateController(IEmployeeDateService employeeDateService, IEmployeeService employeeService)
     {
         _employeeDateService = employeeDateService;
-        //_employeeService = employeeService;
     }
 
     [Authorize(Policy = "AdminOrTalentOrJourneyOrSuperAdminPolicy")]
     [HttpPost]
     public async Task<IActionResult> SaveEmployeeDate([FromBody] EmployeeDateInput employeeDateInput)
     {
-        //var employee = await _employeeService.GetEmployee(employeeDateInput.Email!);
-        //var employeeDateDto = new EmployeeDateDto
-        //{
-        //    Id = 0,
-        //    Employee = employee,
-        //    Subject = employeeDateInput.Subject,
-        //    Note = employeeDateInput.Note,
-        //    Date = employeeDateInput.Date
-        //};
-
-        //await _employeeDateService.Save(employeeDateDto);
-        //return Ok();
         await _employeeDateService.SaveEmployeeDate(employeeDateInput);
         return Ok();
     }
@@ -50,19 +36,6 @@ public class EmployeeDateController : ControllerBase
     [HttpPut]
     public async Task<IActionResult> UpdateEmployeeDate([FromBody] EmployeeDateDto employeeDate)
     {
-        //var employee = await _employeeService.GetEmployee(employeeDate.Employee!.Email!);
-        //var employeeDateDto = new EmployeeDateDto
-        //{
-        //    Id = employeeDate.Id,
-        //    Employee = employee,
-        //    Subject = employeeDate.Subject,
-        //    Note = employeeDate.Note,
-        //    Date = employeeDate.Date
-        //};
-
-        //await _employeeDateService.Update(employeeDate);
-        //return Ok();
-
         await _employeeDateService.UpdateEmployeeDate(employeeDate);
         return Ok();
     }
