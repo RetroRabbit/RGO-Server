e49b41eeb455a8020417069e113809ad172f5c8f
Reino Powell <rpowell@retrorabbit.co.za>
2024-07-12 10:28:09 +0200

    Updated method and parameters for the service unit tests

--- HRIS.Services.Tests/Services/EmployeeDateServiceUnitTests.cs
+++ HRIS.Services.Tests/Services/EmployeeDateServiceUnitTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq.Expressions;
+using HRIS.Models;
+using HRIS.Services.Interfaces;
 using HRIS.Services.Services;
 using Moq;
 using RR.Tests.Data;
@@ -15,15 +17,19 @@ public class EmployeeDateServiceUnitTests
     private readonly Employee _employee;
     private readonly Mock<IUnitOfWork> _mockDb;
     private readonly EmployeeDate _employeeDate;
-    private readonly Mock<IErrorLoggingService> _errorLoggingServiceMock;
+    private readonly EmployeeDateDto _employeeDateDto;
+    private readonly EmployeeDateInput _employeeDateInput;
     private readonly Mock<IEmployeeService> _employeeServiceMock;
 
     public EmployeeDateServiceUnitTests()
     {
         _mockDb = new Mock<IUnitOfWork>();
-        _employeeDateService = new EmployeeDateService(_mockDb.Object);
+        _employeeServiceMock = new Mock<IEmployeeService>();
+        _employeeDateService = new EmployeeDateService(_mockDb.Object, _employeeServiceMock.Object);
         _employee = EmployeeTestData.EmployeeOne;
         _employeeDate = new EmployeeDate { Id = 1, EmployeeId = _employee.Id, Employee = _employee, Subject = "Subject", Note = "Note", Date = new DateOnly() };
+        _employeeDateDto = new EmployeeDate { Id = 1, Employee = _employee, Subject = "Subject", Note = "Note", Date = new DateOnly() }.ToDto();
+        _employeeDateInput = new EmployeeDateInput { Id = 1, Email = "email@gmail.com", Subject = "Subject", Note = "Note", Date = new DateOnly() };
     }
 
     [Fact]
@@ -32,15 +38,7 @@ public class EmployeeDateServiceUnitTests
         _mockDb.Setup(x => x.EmployeeDate.Any(It.IsAny<Expression<Func<EmployeeDate, bool>>>()))
                .ReturnsAsync(false);
 
-        var exists = await _employeeDateService.CheckIfExists(new EmployeeDate
-        {
-            Id = 1,
-            Employee = _employee,
-            Subject = "Subject",
-            Note = "Note",
-            Date = new DateOnly()
-        }.ToDto());
-
+        var exists = await _employeeDateService.CheckIfExists(_employeeDateDto);
         Assert.False(exists);
     }
 
@@ -50,15 +48,7 @@ public class EmployeeDateServiceUnitTests
         _mockDb.Setup(x => x.EmployeeDate.Any(It.IsAny<Expression<Func<EmployeeDate, bool>>>()))
                .ReturnsAsync(true);
 
-        var exists = await _employeeDateService.CheckIfExists(new EmployeeDate
-        {
-            Id = 1,
-            Employee = _employee,
-            Subject = "Subject",
-            Note = "Note",
-            Date = new DateOnly()
-        }.ToDto());
-
+        var exists = await _employeeDateService.CheckIfExists(_employeeDateDto);
         Assert.True(exists);
     }
 
@@ -68,14 +58,7 @@ public class EmployeeDateServiceUnitTests
         _mockDb.Setup(x => x.EmployeeDate.Any(It.IsAny<Expression<Func<EmployeeDate, bool>>>()))
                .ReturnsAsync(true);
 
-        await Assert.ThrowsAsync<CustomException>(() => _employeeDateService.Save(new EmployeeDate
-        {
-            Id = 1,
-            Employee = _employee,
-            Subject = "Subject",
-            Note = "Note",
-            Date = new DateOnly()
-        }.ToDto()));
+        await Assert.ThrowsAsync<CustomException>(() => _employeeDateService.SaveEmployeeDate(_employeeDateInput));
     }
 
     [Fact]
@@ -84,8 +67,7 @@ public class EmployeeDateServiceUnitTests
         _mockDb.Setup(x => x.EmployeeDate.Any(It.IsAny<Expression<Func<EmployeeDate, bool>>>()))
                .ReturnsAsync(false);
 
-        await _employeeDateService.Save(_employeeDate.ToDto());
-
+        await _employeeDateService.SaveEmployeeDate(_employeeDateInput);
         _mockDb.Verify(x => x.EmployeeDate.Add(It.IsAny<EmployeeDate>()), Times.Once);
     }
 
@@ -95,14 +77,7 @@ public class EmployeeDateServiceUnitTests
         _mockDb.Setup(x => x.EmployeeDate.Any(It.IsAny<Expression<Func<EmployeeDate, bool>>>()))
                .ReturnsAsync(false);
 
-        await Assert.ThrowsAsync<CustomException>(() => _employeeDateService.Update(new EmployeeDate
-        {
-            Id = 1,
-            Employee = _employee,
-            Subject = "Subject",
-            Note = "Note",
-            Date = new DateOnly()
-        }.ToDto()));
+        await Assert.ThrowsAsync<CustomException>(() => _employeeDateService.UpdateEmployeeDate(_employeeDate.ToDto()));
     }
 
     [Fact]
@@ -111,15 +86,7 @@ public class EmployeeDateServiceUnitTests
         _mockDb.Setup(x => x.EmployeeDate.Any(It.IsAny<Expression<Func<EmployeeDate, bool>>>()))
                .ReturnsAsync(true);
 
-        await _employeeDateService.Update(new EmployeeDate
-        {
-            Id = 1,
-            Employee = _employee,
-            Subject = "Subject",
-            Note = "Note",
-            Date = new DateOnly()
-        }.ToDto());
-
+        await _employeeDateService.UpdateEmployeeDate(_employeeDateDto);
         _mockDb.Verify(x => x.EmployeeDate.Update(It.IsAny<EmployeeDate>()), Times.Once);
     }
 
@@ -133,7 +100,6 @@ public class EmployeeDateServiceUnitTests
                .Returns(_employeeDate.ToMockIQueryable());
 
         await _employeeDateService.Delete(1);
-
         _mockDb.Verify(x => x.EmployeeDate.Delete(It.IsAny<int>()), Times.Once);
     }
 
@@ -153,7 +119,6 @@ public class EmployeeDateServiceUnitTests
                .Returns(_employeeDate.ToMockIQueryable());
 
         await _employeeDateService.Get(_employeeDate.ToDto());
-
         _mockDb.Verify(x => x.EmployeeDate.Get(It.IsAny<Expression<Func<EmployeeDate, bool>>>()), Times.Once);
     }
 
