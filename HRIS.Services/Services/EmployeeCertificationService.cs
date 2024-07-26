377ba5257052bec8028d3942809a0ce2e9b5ad6c
KayembaJonathan <kayembejonathan5@gmail.com>
2024-07-26 07:53:34 +0200

    Update HRIS.Services/Services/EmployeeCertificationService.cs
    Co-authored-by: Brandon <bseptember@retrorabbit.co.za>

--- HRIS.Services/Services/EmployeeCertificationService.cs
+++ HRIS.Services/Services/EmployeeCertificationService.cs
@@ -33,10 +33,8 @@ public class EmployeeCertificationService : IEmployeeCertificationService
         if (!exists)
             throw new CustomException("Certificate not found");
 
-        if (_identity.Role is not ("SuperAdmin" or "Admin" or "Talent" or "Journey") && employeeCertificationDto.Id != _identity.EmployeeId)
-        {
+        if (!_identity.IsSupport && employeeCertificationDto.Id != _identity.EmployeeId)
             throw new CustomException("Unauthorized access.");
-        }
 
         return (await _db.EmployeeCertification.Add(new EmployeeCertification(employeeCertificationDto))).ToDto();
     }
