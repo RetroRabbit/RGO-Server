ca841d2a68ffc8f89d82adc0eb6c86a3c4d6599e
Reino Powell <rpowell@retrorabbit.co.za>
2024-08-01 11:14:20 +0200

    Update method removed the try catch block and changed role to isSupport

--- RR.App/Controllers/HRIS/WorkExperienceController.cs
+++ RR.App/Controllers/HRIS/WorkExperienceController.cs
@@ -1,5 +1,6 @@
 ﻿using HRIS.Models;
 using HRIS.Services.Interfaces;
+using HRIS.Services.Services;
 using HRIS.Services.Session;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
