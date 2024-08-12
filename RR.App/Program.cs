6b9c6889fa558f445b3ac628ad0792fb9917583f
EstiaanBritz <82169901+madwizzard@users.noreply.github.com>
2024-08-12 15:23:05 +0200

    removing comment

--- RR.App/Program.cs
+++ RR.App/Program.cs
@@ -25,7 +25,6 @@ namespace RR.App
 
             builder.Services.Configure<AuthManagement>(builder.Configuration.GetSection("AuthManagement"));
             builder.Services.Configure<SMTPSettings>(builder.Configuration.GetSection("SMTP"));
-            //builder.Services.AddAutoMapper(typeof(Program));
 
             SetupConfiguration(configuration);
             SetupDependencyInjection(builder.Services, configuration);
