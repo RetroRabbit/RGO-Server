36ebe935230e023a977e0e3c021733a29f766c5c
MathildaShongwe15 <156072510+MathildaShongwe15@users.noreply.github.com>
2024-08-13 09:11:51 +0200

    Update RR.Tests.Data/Models/HRIS/EmployeeTestData.cs
    Co-authored-by: EstiaanBritz <82169901+madwizzard@users.noreply.github.com>

--- RR.Tests.Data/Models/HRIS/EmployeeTestData.cs
+++ RR.Tests.Data/Models/HRIS/EmployeeTestData.cs
@@ -41,8 +41,6 @@ public class EmployeeTestData
         CellphoneNo = "0000000000",
         ClientAllocated = null,
         TeamLead = null,
-        //PhysicalAddress = EmployeeAddressTestData.EmployeeAddressOne,
-        //PostalAddress = EmployeeAddressTestData.EmployeeAddressOne,
         HouseNo = null,
         EmergencyContactName = null,
         EmergencyContactNo = null,
