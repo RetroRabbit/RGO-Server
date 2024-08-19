dd9b352150ab0777e84ccf929ee52ef168ebe341
MathildaShongwe15 <156072510+MathildaShongwe15@users.noreply.github.com>
2024-08-19 08:44:34 +0200

    Update RR.App/Controllers/HRIS/BankingAndStarterKitController.cs
    Co-authored-by: EstiaanBritz <82169901+madwizzard@users.noreply.github.com>

--- RR.App/Controllers/HRIS/BankingAndStarterKitController.cs
+++ RR.App/Controllers/HRIS/BankingAndStarterKitController.cs
@@ -13,7 +13,6 @@ public class BankingAndStarterKitController : ControllerBase
     public BankingAndStarterKitController(IBankingAndStarterKitService bankingAndStarterKitService)
     {
         _bankingAndStarterKitService = bankingAndStarterKitService ;
-
     }
 
     [Authorize(Policy = "AllRolesPolicy")]
