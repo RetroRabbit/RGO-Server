1a99e3d8af9a9dac092552da8080d512600f9fed
Wimpie Scholtz <wscholtz@retrorabbit.co.za>
2024-08-19 16:26:55 +0200

    Mostly refactored + unit tests WIP

--- HRIS.Services.Tests/Services/PropertyAccessServiceUnitTests.cs
+++ HRIS.Services.Tests/Services/PropertyAccessServiceUnitTests.cs
@@ -16,19 +16,59 @@ public class PropertyAccessServiceUnitTests
 {
     private readonly Mock<IUnitOfWork> _dbMock;
     private readonly Mock<IPropertyAccessService> _propertyAccessService;
+    private readonly PropertyAccessService propertyAccessService;
+    private readonly PropertyAccessService propertyAccessService2;
+
+    private readonly List<RoleDto> roleList;
+    private readonly List<EmployeeRole> employeeRoleList;
 
     public PropertyAccessServiceUnitTests()
     {
         _dbMock = new Mock<IUnitOfWork>();
         _propertyAccessService = new Mock<IPropertyAccessService>();
-        propertyAccessService = new PropertyAccessService(_dbMock.Object);
+        propertyAccessService = new PropertyAccessService(_dbMock.Object, new AuthorizeIdentityMock("test@gmail.com", "test", "Admin", 1));
+        propertyAccessService2 = new PropertyAccessService(_dbMock.Object, new AuthorizeIdentityMock("test@gmail.com", "test", "Employee", 2));
 
+        var testEmployee = EmployeeTestData.EmployeeOne;
+        roleList = new List<RoleDto>
+        {
+            new () {Id = 1, Description = "Admin" },
+            new () { Id = 2, Description = "Manager" },
+            new () { Id = 3, Description = "Employee" },
+            new () { Id = 4, Description = "Intern" }
+        };
+
+        employeeRoleList = new List<EmployeeRole>
+        {
+            new()
+            {
+                Id = 1,
+                EmployeeId = testEmployee.Id,
+                RoleId = roleList[0].Id,
+                Employee = testEmployee,
+                Role = new Role(roleList[0])
+            },
+            new()
+            {
+                Id = 2,
+                EmployeeId = testEmployee.Id,
+                RoleId = roleList[1].Id,
+                Employee = testEmployee,
+                Role = new Role(roleList[1])
+            },
+            new()
+            {
+                Id = 3,
+                EmployeeId = testEmployee.Id,
+                RoleId = roleList[2].Id,
+                Employee = testEmployee,
+                Role = new Role(roleList[2])
+            }
+        };
     }
 
-    public readonly PropertyAccessService propertyAccessService;
-
     [Fact]
-    public async Task GetAllTest()
+    public async Task GetAllTestPass()
     {
         _dbMock.Setup(p => p.PropertyAccess.Get(It.IsAny<Expression<Func<PropertyAccess, bool>>>()))
                .Returns(PropertyAccessTestData.PropertyAccessList.ToMockIQueryable());
@@ -42,21 +82,60 @@ public class PropertyAccessServiceUnitTests
     }
 
     [Fact]
-    public void GetAccessListByEmployeeIdTest()
+    public void GetAccessListByEmployeeIdTestPass()
     {
         _dbMock.Setup(e => e.PropertyAccess.Get(It.IsAny<Expression<Func<PropertyAccess, bool>>>()))
                .Returns(PropertyAccessTestData.PropertyAccessList.ToMockIQueryable());
 
-        var result = propertyAccessService.GetAccessListByEmployeeId(1);
+        _dbMock.Setup(e => e.EmployeeRole.Get(It.IsAny<Expression<Func<EmployeeRole, bool>>>()))
+               .Returns(employeeRoleList[0].ToMockIQueryable());
+
+        _dbMock
+            .Setup(r => r.Employee.Any(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .ReturnsAsync(true);
+
+        _dbMock
+            .Setup(r => r.Role.Any(It.IsAny<Expression<Func<Role, bool>>>()))
+            .ReturnsAsync(true);
+
+        _dbMock.Setup(e => e.PropertyAccess.GetAll(It.IsAny<Expression<Func<PropertyAccess, bool>>>()))
+               .ReturnsAsync(PropertyAccessTestData.PropertyAccessOne.ToMockIQueryable().ToList());
+
+        var result = propertyAccessService.GetAccessListByEmployeeId(1); 
 
         Assert.NotNull(result);
+        Assert.Equivalent(PropertyAccessTestData.PropertyAccessOne.ToDto(), result.Result[0]);
     }
-    [Fact]
-    public void GetAccessListByRoleIdTest()
-    {
-        _dbMock.Setup(e => e.PropertyAccess.Get(It.IsAny<Expression<Func<PropertyAccess, bool>>>()))
-               .Returns(PropertyAccessTestData.PropertyAccessList.ToMockIQueryable());
 
+    [Fact]
+    public async void GetAccessListByEmployeeIdTestFail()
+    {
+        _dbMock
+            .Setup(r => r.Employee.Any(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .ReturnsAsync(false);
+
+        await Assert.ThrowsAsync<CustomException>(() => propertyAccessService2.GetAccessListByEmployeeId(1));
+    }
+
+    [Fact]
+    public async void GetAccessListByEmployeeIdTestUnauthorised()
+    {
+        _dbMock
+            .Setup(r => r.Employee.Any(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .ReturnsAsync(true);
+
+        await Assert.ThrowsAsync<CustomException>(() => propertyAccessService2.GetAccessListByEmployeeId(1));
+    }
+
+    [Fact]
+    public void GetAccessListByRoleIdTestPass()
+    {
+        _dbMock.Setup(e => e.PropertyAccess.GetAll(It.IsAny<Expression<Func<PropertyAccess, bool>>>()))
+               .ReturnsAsync(PropertyAccessTestData.PropertyAccessList.ToMockIQueryable().ToList());
+
+        _dbMock
+            .Setup(r => r.Role.Any(It.IsAny<Expression<Func<Role, bool>>>()))
+            .ReturnsAsync(true);
 
         var result = propertyAccessService.GetAccessListByRoleId(1);
 
@@ -64,13 +143,17 @@ public class PropertyAccessServiceUnitTests
     }
 
     [Fact]
-    public async Task UpdatePropertyAccess()
+    public async Task UpdatePropertyAccessPass()
     {
         _propertyAccessService.Setup(r => r.UpdatePropertyAccess(PropertyAccessTestData.PropertyAccessOne.Id, PropertyAccessLevel.read));
-                              
+
+        _dbMock
+            .Setup(r => r.PropertyAccess.Any(It.IsAny<Expression<Func<PropertyAccess, bool>>>()))
+            .ReturnsAsync(true);
+
         _dbMock.Setup(r => r.PropertyAccess.Update(It.IsAny<PropertyAccess>()))
                .ReturnsAsync(PropertyAccessTestData.PropertyAccessOne);
 
-            await propertyAccessService.UpdatePropertyAccess(PropertyAccessTestData.PropertyAccessOne.Id, PropertyAccessLevel.read);
+        await propertyAccessService.UpdatePropertyAccess(PropertyAccessTestData.PropertyAccessOne.Id, PropertyAccessLevel.read);
     }
 }
--- HRIS.Services/Interfaces/IPropertyAccessService.cs
+++ HRIS.Services/Interfaces/IPropertyAccessService.cs
@@ -12,18 +12,21 @@ public interface IPropertyAccessService
     ///// <param name="employeeId"></param>
     ///// <returns>dictionary of properties with the access level for selected user</returns>
     Task<List<PropertyAccessDto>> GetAccessListByEmployeeId(int employeeId);
+
     ///// <summary>
     /////     Get's all the Property that a user has access to based on role
     ///// </summary>
     ///// <param name="email"></param>
     ///// <returns>dictionary of properties with the access level for selected user</returns>
     Task<List<PropertyAccessDto>> GetAccessListByRoleId(int roleId);
+
     ///// <summary>
     /////     Get's all the Properties
     ///// </summary>
     ///// <param name=""></param>
     ///// <returns>dictionary of properties with the access level for all user</returns>
     Task<List<PropertyAccessDto>> GetAll();
+
     ///// <summary>
     /////     updates a single properties access level
     ///// </summary>
@@ -31,11 +34,33 @@ public interface IPropertyAccessService
     ///// <param name="propertyAccess"></param>
     ///// <returns></returns>
     Task UpdatePropertyAccess(int propertyId, PropertyAccessLevel propertyAccess);
+
     ///// <summary>
     /////    Creates all properties and updates if a new table is added to the list
     ///// </summary>
     ///// <param name="email"></param>
     ///// <returns></returns>
     Task CreatePropertyAccessEntries();
+
+    //// <summary>
+    //// Check if Employee exists by ID
+    //// </summary>
+    //// <param name="employeeId"></param>
+    //// <returns></returns>
+    Task<bool> CheckEmployee(int employeeId);
+
+    //// <summary>
+    //// Check if Role Exists by ID
+    //// </summary>
+    //// <param name="roleId"></param>
+    //// <returns></returns>
+    Task<bool> CheckRoleById(int roleId);
+
+    //// <summary>
+    //// Check if the Property Access exists by ID
+    //// </summary>
+    //// <param name="id"></param>
+    //// <returns></returns>
+    Task<bool> CheckPropertyAccess(int id);
 
 }
