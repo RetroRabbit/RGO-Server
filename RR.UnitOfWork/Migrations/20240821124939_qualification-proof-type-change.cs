5e188e1f1b5a028ca2aa873f4b9eb636ca3bd7ec
Reino Powell <rpowell@retrorabbit.co.za>
2024-08-21 19:24:14 +0200

    Added migration

---
+++ RR.UnitOfWork/Migrations/20240821124939_qualification-proof-type-change.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RR.UnitOfWork.Migrations
+{
+    /// <inheritdoc />
+    public partial class qualificationprooftypechange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "proofOfQualification",
+                table: "EmployeeQualifications",
+                type: "bytea",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "proofOfQualification",
+                table: "EmployeeQualifications",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+        }
+    }
+}
