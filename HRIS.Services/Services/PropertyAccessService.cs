bff4c86d6b86f208f92890ad95aa58f86410853f
EstiaanBritz <82169901+madwizzard@users.noreply.github.com>
2024-08-29 13:17:00 +0200

    adding employee qualification and salary details to property Access management

--- HRIS.Services/Services/PropertyAccessService.cs
+++ HRIS.Services/Services/PropertyAccessService.cs
@@ -76,7 +76,7 @@ public class PropertyAccessService : IPropertyAccessService
             throw new CustomException("Unauthorized Access.");
 
         var currentAccessProperties = await _db.PropertyAccess.GetAll();
-        var tables = new List<string> { "Employee", "EmployeeData", "EmployeeRole", "EmployeeAddress", "EmployeeBanking" };
+        var tables = new List<string> { "Employee", "EmployeeData", "EmployeeRole", "EmployeeAddress", "EmployeeBanking", "EmployeeQualification", "EmployeeSalaryDetails" };
         var roles = await _db.Role.GetAll();
         var properties = new List<PropertyAccess>();
 
