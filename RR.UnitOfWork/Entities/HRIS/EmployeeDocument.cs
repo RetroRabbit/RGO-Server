02e0ca7108f36ae9e67f960f60558463813be591
Brandon <bseptember@retrorabbit.co.za>
2024-09-05 15:04:48 +0200

    fix: copy paste issue

--- RR.UnitOfWork/Entities/HRIS/EmployeeDocument.cs
+++ RR.UnitOfWork/Entities/HRIS/EmployeeDocument.cs
@@ -1,12 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using HRIS.Models;
-using HRIS.Models.Enums;
-using RR.UnitOfWork.Interfaces;
-
-namespace RR.UnitOfWork.Entities.HRIS;
-
-[Table("EmployeeDocument")]using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using HRIS.Models;
 using HRIS.Models.Enums;
@@ -15,6 +7,7 @@ using RR.UnitOfWork.Interfaces;
 namespace RR.UnitOfWork.Entities.HRIS;
 
 [Table("EmployeeDocument")]
+
 public class EmployeeDocument : IModel
 {
     public EmployeeDocument()
@@ -54,83 +47,6 @@ public class EmployeeDocument : IModel
     [Column("adminFileCategory")] public AdminFileCategory AdminFileCategory { get; set; }
 
     [Column("blob")] public byte[] Blob { get; set; } = Array.Empty<byte>();
-
-    [Column("status")] public DocumentStatus? Status { get; set; }
-
-    [Column("uploadDate")] public DateTime UploadDate { get; set; }
-
-    [Column("reason")] public string? Reason { get; set; }
-
-    [Column("counterSign")] public bool CounterSign { get; set; }
-
-    [Column("documentType")] public DocumentType? DocumentType { get; set; }
-    
-    [Column("lastUpdatedDate")] public DateTime LastUpdatedDate { get; set; }
-
-    public virtual Employee? Employee { get; set; }
-
-    [Key] [Column("id")] public int Id { get; set; }
-
-    public EmployeeDocumentDto ToDto()
-    {
-        return new EmployeeDocumentDto {
-                                       Id = Id,
-                                       EmployeeId = EmployeeId,
-                                       Reference = Reference,
-                                       FileName = FileName,
-                                       FileCategory = FileCategory,
-                                       DocumentType = DocumentType,
-                                       EmployeeFileCategory = EmployeeFileCategory,
-                                       AdminFileCategory = AdminFileCategory,
-                                       Blob = Blob,
-                                       Status = Status,
-                                       UploadDate = UploadDate,
-                                       Reason = Reason,
-                                       CounterSign = CounterSign,
-                                       LastUpdatedDate = LastUpdatedDate
-        };
-    }
-}
-
-public class EmployeeDocument : IModel
-{
-    public EmployeeDocument()
-    {
-    }
-
-    public EmployeeDocument(EmployeeDocumentDto employeeDocumentsDto)
-    {
-        Id = employeeDocumentsDto.Id;
-        EmployeeId = employeeDocumentsDto.EmployeeId!;
-        Reference = employeeDocumentsDto.Reference;
-        FileName = employeeDocumentsDto.FileName;
-        FileCategory = employeeDocumentsDto.FileCategory;
-        EmployeeFileCategory = employeeDocumentsDto.EmployeeFileCategory;
-        AdminFileCategory = employeeDocumentsDto.AdminFileCategory;
-        Blob = employeeDocumentsDto.Blob;
-        Status = employeeDocumentsDto.Status;
-        UploadDate = employeeDocumentsDto.UploadDate;
-        Reason = employeeDocumentsDto.Reason;
-        CounterSign = employeeDocumentsDto.CounterSign;
-        DocumentType = employeeDocumentsDto?.DocumentType;
-        LastUpdatedDate = employeeDocumentsDto.LastUpdatedDate;
-    }
-
-    [Column("employeeId")]
-    [ForeignKey("Employee")]
-    public int EmployeeId { get; set; }
-
-    [Column("reference")] public string? Reference { get; set; }
-
-    [Column("fileName")] public string? FileName { get; set; }
-
-    [Column("fileCategory")] public FileCategory FileCategory { get; set; }
-
-    [Column("employeeFileCategory")] public EmployeeFileCategory EmployeeFileCategory { get; set; }
-
-    [Column("adminFileCategory")] public AdminFileCategory AdminFileCategory { get; set; }
-
-    [Column("blob")] public string? Blob { get; set; }
 
     [Column("status")] public DocumentStatus? Status { get; set; }
 
